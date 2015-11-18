4483d16c3a7b5c40b1ae958e1691bc5e3136110a
Michael Simons <msimons@microsoft.com>
2015-11-18 07:43:08 -0600

    Updating artifacts to reference the microsoft/dotnet docker hub repo

--- build.sh
+++ build.sh
@@ -13,12 +13,12 @@ fi
 echo Building Docker image: dotnet
 docker build -t dotnet --no-cache ./$VERSION/
 docker tag -f dotnet dotnet:$VERSION
-docker tag -f dotnet msimons/dotnet
-docker tag -f dotnet msimons/dotnet:$VERSION
+docker tag -f dotnet microsoft/dotnet
+docker tag -f dotnet microsoft/dotnet:$VERSION
 
 echo Building Docker image: dotnet:onbuild
 docker build -t dotnet:onbuild --no-cache ./$VERSION/onbuild
 docker tag -f dotnet:onbuild dotnet:${VERSION}-onbuild
-docker tag -f dotnet:onbuild msimons/dotnet:onbuild
-docker tag -f dotnet:onbuild msimons/dotnet:${VERSION}-onbuild
+docker tag -f dotnet:onbuild microsoft/dotnet:onbuild
+docker tag -f dotnet:onbuild microsoft/dotnet:${VERSION}-onbuild
 
