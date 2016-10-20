48afa28ac0a1a5967f42c249c3e97c694e05272e
Michael Simons (VISUAL STUDIO) <msimons@microsoft.com>
2016-10-20 10:07:02 -0500

    Enable CI on Windows

--- build-and-test.ps1
+++ build-and-test.ps1
@@ -5,9 +5,9 @@ $dockerRepo="microsoft/dotnet"
 
 pushd $PSScriptRoot
 
-Get-ChildItem -Recurse -Filter Dockerfile | where DirectoryName -like "*\nanoserver*" | sort DirectoryName | foreach {
+Get-ChildItem -Recurse -Filter Dockerfile | where {$_.DirectoryName.TrimStart($PSScriptRoot) -like "*\nanoserver*"} | sort DirectoryName | foreach {
     $tag = "$($dockerRepo):" + $_.DirectoryName.Replace($PSScriptRoot, '').TrimStart('\').Replace('\', '-') -replace "nanoserver$", "nanoserver-sdk"
-    Write-Host "----- Building $tag -----"
+    Write-Host "--- Building $tag from $($_.DirectoryName) ---"
     docker build --no-cache -t $tag $_.DirectoryName
     if (-NOT $?) {
         throw "Failed building $tag"
--- netci.groovy
+++ netci.groovy
@@ -3,11 +3,12 @@ import jobs.generation.Utilities
 def project = GithubProject
 def branch = GithubBranchName
 def isPR = true
-def platformList = ['Ubuntu16.04:Debian'/*, 'Windows_2016:NanoServer'*/]
+def platformList = ['Ubuntu16.04:Debian', 'Windows_2016:NanoServer']
 
 platformList.each { platform ->
     def(hostOS, containerOS) = platform.tokenize(':')
     def newJobName = Utilities.getFullJobName(project, containerOS, isPR)
+    def machineLabel = (hostOS == 'Windows_2016') ? 'latest-containers' : 'latest-or-auto-docker'
 
     def newJob = job(newJobName) {
         steps {
@@ -20,7 +21,7 @@ platformList.each { platform ->
         }
     }
 
-    Utilities.setMachineAffinity(newJob, hostOS, 'latest-or-auto-docker')
+    Utilities.setMachineAffinity(newJob, hostOS, machineLabel)
     Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
     Utilities.addGithubPRTriggerForBranch(newJob, branch, containerOS)
 }
