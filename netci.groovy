c05447f9451c9ea5e39698a51bd47f80193cf200
MichaelSimons <msimons@microsoft.com>
2017-07-18 11:19:34 -0500

    Broke apart CI into more jobs

--- netci.groovy
+++ netci.groovy
@@ -4,24 +4,28 @@ def project = GithubProject
 def branch = GithubBranchName
 def isPR = true
 def platformList = ['Ubuntu16.04:Debian', 'Windows_2016:NanoServer']
+def versionList = ['1.0', '1.1', '2.0']
 
 platformList.each { platform ->
     def(hostOS, containerOS) = platform.tokenize(':')
-    def newJobName = Utilities.getFullJobName(project, containerOS, isPR)
     def machineLabel = (hostOS == 'Windows_2016') ? 'latest-containers' : 'latest-or-auto-docker'
 
-    def newJob = job(newJobName) {
-        steps {
-            if (hostOS == 'Windows_2016') {
-                batchFile("powershell -NoProfile -Command .\\build-and-test.ps1")
-            }
-            else {
-                shell("docker build --rm -t testrunner -f ./test/Dockerfile.linux.testrunner . && docker run -v /var/run/docker.sock:/var/run/docker.sock testrunner powershell -File build-and-test.ps1")
+    versionList.each { version ->
+        def newJobName = Utilities.getFullJobName(project, "${containerOS}_${version}", isPR)
+
+        def newJob = job(newJobName) {
+            steps {
+                if (hostOS == 'Windows_2016') {
+                    batchFile("powershell -NoProfile -Command .\\build-and-test.ps1 -Filter ${version}")
+                }
+                else {
+                    shell("docker build --rm -t testrunner -f ./test/Dockerfile.linux.testrunner . && docker run -v /var/run/docker.sock:/var/run/docker.sock testrunner powershell -File build-and-test.ps1 -Filter ${version}")
+                }
             }
         }
-    }
 
-    Utilities.setMachineAffinity(newJob, hostOS, machineLabel)
-    Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
-    Utilities.addGithubPRTriggerForBranch(newJob, branch, containerOS)
+        Utilities.setMachineAffinity(newJob, hostOS, machineLabel)
+        Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+        Utilities.addGithubPRTriggerForBranch(newJob, branch, "${containerOS} - ${version} Dockerfiles")
+    }
 }
