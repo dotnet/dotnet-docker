165f6adf4df0b648c742e78e7ddc1d3a2b4e69f9
Ravi Eda <raeda@microsoft.com>
2017-10-10 09:26:00 -0500

    Stop producing Nano Server Version suffixed tags.

---
+++ scripts/Get-TagsDocumentation.ps1
@@ -0,0 +1,30 @@
+param(
+    [string]$Branch='master',
+    [string]$ImageBuilderImageName='microsoft/dotnet-buildtools-prereqs:image-builder-jessie-20171005132855',
+    [string]$RepoName
+)
+
+$ErrorActionPreference = 'Stop'
+$repoRoot = Split-Path -Path "$PSScriptRoot" -Parent
+
+if ([String]::IsNullOrWhiteSpace($RepoName))
+{
+    $remoteUrl = $null
+    if ([Uri]::TryCreate(((git config --get remote.origin.url) | Out-String), [UriKind]::Absolute, [ref]$remoteUrl))
+    {
+        $RepoName = [System.IO.Path]::GetFileNameWithoutExtension($remoteUrl.ToString())
+    }
+    if ([String]::IsNullOrWhiteSpace($RepoName))
+    {
+        Write-Error 'Could not automatically determine repository name. Add -RepoName <REPO> to override.'
+    }
+}
+
+& docker pull $ImageBuilderImageName
+
+& docker run --rm `
+    -v /var/run/docker.sock:/var/run/docker.sock `
+    -v "${repoRoot}:/repo" `
+    -w /repo `
+    $ImageBuilderImageName `
+    generateTagsReadme "https://github.com/dotnet/${RepoName}/blob/${Branch}"
