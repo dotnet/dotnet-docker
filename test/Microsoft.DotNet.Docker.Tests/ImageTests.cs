2d53a3de0a131be6f471aca66739029b59e6f638
Michael Simons <msimons@microsoft.com>
2018-02-26 08:39:59 -0600

    Add Alpine SDK image (#376)

--- test/Microsoft.DotNet.Docker.Tests/ImageTests.cs
+++ test/Microsoft.DotNet.Docker.Tests/ImageTests.cs
@@ -27,7 +27,7 @@ namespace Microsoft.DotNet.Docker.Tests
                 new ImageDescriptor { DotNetCoreVersion = "2.0", OsVariant = OS.Stretch, HasNoSdk = true, Architecture = "arm" },
                 new ImageDescriptor { DotNetCoreVersion = "2.1", RuntimeDepsVersion = "2.0" },
                 new ImageDescriptor { DotNetCoreVersion = "2.1", RuntimeDepsVersion = "2.0", OsVariant = OS.Jessie },
-                new ImageDescriptor { DotNetCoreVersion = "2.1", OsVariant = OS.Alpine, HasNoSdk = true },
+                new ImageDescriptor { DotNetCoreVersion = "2.1", OsVariant = OS.Alpine },
                 new ImageDescriptor
                 {
                     DotNetCoreVersion = "2.1",
