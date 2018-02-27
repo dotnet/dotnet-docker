ba29124331dbf81bdaa1b17f84cc5d3991198298
MichaelSimons <msimons@microsoft.com>
2018-02-26 20:50:29 -0600

    Add infrastructure to build and push samples to Docker Hub

--- scripts/Get-TagsDocumentation.ps1
+++ scripts/Get-TagsDocumentation.ps1
@@ -1,5 +1,6 @@
 param(
     [string]$Branch='master',
+    [string]$Manifest='manifest.json',
     [string]$ImageBuilderImageName='microsoft/dotnet-buildtools-prereqs:image-builder-jessie-20171122115946',
     [string]$RepoName
 )
@@ -27,4 +28,4 @@ if ([String]::IsNullOrWhiteSpace($RepoName))
     -v "${repoRoot}:/repo" `
     -w /repo `
     $ImageBuilderImageName `
-    generateTagsReadme --update-readme "https://github.com/dotnet/${RepoName}/blob/${Branch}"
+    generateTagsReadme --update-readme --manifest ${Manifest} "https://github.com/dotnet/${RepoName}/blob/${Branch}"
