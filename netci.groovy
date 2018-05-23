32fa4a4012366d033c8e288ce62da120bb6435b9
Matt Galbraith <MattGal@users.noreply.github.com>
2018-05-23 11:26:34 -0700

    Add Server RS4 support

--- netci.groovy
+++ netci.groovy
@@ -3,13 +3,16 @@ import jobs.generation.Utilities
 def project = GithubProject
 def branch = GithubBranchName
 def isPR = true
-def platformList = ['Ubuntu16.04:Debian', 'Windows_2016:NanoServer-sac2016', 'Windows_2016:NanoServer-1709']
+def platformList = ['Ubuntu16.04:Debian', 'Windows_2016:NanoServer-sac2016', 'Windows_2016:NanoServer-1709', 'Windows_2016:NanoServer-1803']
 
 platformList.each { platform ->
     def(hostOS, containerOS) = platform.tokenize(':')
     def machineLabel = (hostOS == 'Windows_2016') ? 'latest-docker' : 'latest-or-auto-docker'
 
-    if (containerOS == 'NanoServer-1709') {
+    if (containerOS == 'NanoServer-1803') {
+        versionList = ['2.0', '2.1']
+    }
+    else if (containerOS == 'NanoServer-1709') {
         versionList = ['2.0', '2.1']
     }
     else if (containerOS == 'NanoServer-sac2016') {
@@ -37,6 +40,9 @@ platformList.each { platform ->
         if (containerOS == 'NanoServer-1709') {
             newJob.with {label('windows.10.amd64.serverrs3.open')}
         }
+        else if (containerOS == 'NanoServer-1803') {
+            newJob.with {label('windows.10.amd64.serverrs4.open')}
+        }    
         else {
             Utilities.setMachineAffinity(newJob, hostOS, machineLabel)
         }
