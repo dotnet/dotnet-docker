89965c3e235550411d6b7e9af69d419105b7e1f0
Richard Lander <rlander@microsoft.com>
2018-06-13 07:38:02 -0700

    Add Join-Path to script

--- samples/dotnetapp/build.ps1
+++ samples/dotnetapp/build.ps1
@@ -9,42 +9,51 @@ function Log {
     Write-Output "###### $s"
 }
 
+function Check {
+    Param ([string] $s)
+    if ($LASTEXITCODE -ne 0) { throw "###### $s failed ######" }
+}
+
 $IsRunningOnUnix = $PSVersionTable.contains("Platform") -and $PSVersionTable.Platform -eq "Unix"
 $DockerOS = docker version -f "{{ .Server.Os }}"
 $ImageName = "dotnetapp"
 $TestImageName = "dotnetapp:test"
 
-Log "Running Docker build"
+Log "Building docker image"
 docker build --pull -t $ImageName .
-if ($LASTEXITCODE -ne 0) { throw "## docker build failed" }
+Check "docker build failed"
 
-Log "Run tests and collect results."
+Log "Building test docker image."
 docker build --pull --target testrunner -t $TestImageName .
-if ($LASTEXITCODE -ne 0) { throw "## docker build failed" }
+Check "docker build failed"
 
-if (!(Test-Path -Path "$PSScriptRoot\TestResults")) {
-    Log "Create TestResults folder"
-    mkdir $PSScriptRoot\TestResults
+$TestResultsName = "TestResults"
+$TestResults = Join-Path $PSScriptRoot $TestResultsName
+$TestResultsExists = (Test-Path -Path $TestResults)
+
+Log "Check if $TestResultsName directory exists: $TestResultsExists"
+
+if (!$TestResultsExists) {
+    Log "Create $TestResultsName folder"
+    mkdir $TestResults
+    gci . TestResults -ad
 }
 
 if ($IsRunningOnUnix) {
     Log "Environment: Unix containers"
-    docker run --rm -v $PSScriptRoot/TestResults:/app/tests/TestResults $TestImageName
+    docker run --rm -v ${TestResults}:/app/tests/${TestResultsName} $TestImageName
 }
 elseif ($DockerOS -eq "linux") {
     Log "Environment: Unix containers on Windows"
-    docker run --rm -v $PSScriptRoot\TestResults:/app/tests/TestResults $TestImageName        
+    docker run --rm -v ${TestResults}:/app/tests/${TestResultsName} $TestImageName        
 }
 else {
     Log "Environment: Windows containers"
-    docker run --rm -v $PSScriptRoot\TestResults:C:\app\tests\TestResults $TestImageName
+    docker run --rm -v ${TestResults}:C:\app\tests\${TestResultsName} $TestImageName
 }
 
-if ($LASTEXITCODE -ne 0) { Log "Tests failed" }
-
-
-$testfiles = gci $PSScriptRoot\TestResults\*.trx | sort LastWriteTime | select -last 1
+$testfiles = gci $TestResults *.trx | Sort-Object -Property LastWriteTime | select -last 1
 
 Log "Docker image built: $ImageName"
-Log "Test file:"
+Log "Test log file:"
 Write-Output $testfiles
