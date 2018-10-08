b8fb99df7fc56344e61ac6e42fd83cc8de679f48
MichaelSimons <msimons@microsoft.com>
2018-10-08 12:48:41 -0500

    Revert "[nightly] Add support for Windows Server 2019 (#720)"
    This reverts commit 8b826b5aeaae27873236cf2439a33a5c34a9dd14.

--- tests/Microsoft.DotNet.Docker.Tests/OS.cs
+++ tests/Microsoft.DotNet.Docker.Tests/OS.cs
@@ -15,6 +15,5 @@ namespace Microsoft.DotNet.Docker.Tests
         public const string NanoServerSac2016 = "nanoserver-sac2016";
         public const string NanoServer1709 = "nanoserver-1709";
         public const string NanoServer1803 = "nanoserver-1803";
-        public const string NanoServer1809 = "nanoserver-1809";
     }
 }
