dc15f6793b9cde40b895540695e3659c3ab58a82
MichaelSimons <msimons@microsoft.com>
2018-11-07 19:02:57 -0600

    Refactor ImageData to be more strongly typed

---
+++ tests/Microsoft.DotNet.Docker.Tests/ImageVersion.cs
@@ -0,0 +1,17 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace Microsoft.DotNet.Docker.Tests
+{
+    public static class ImageVersion
+    {
+        public static readonly Version V1_0 = new Version(1, 0);
+        public static readonly Version V1_1 = new Version(1, 1);
+        public static readonly Version V2_1 = new Version(2, 1);
+        public static readonly Version V2_2 = new Version(2, 2);
+        public static readonly Version V3_0 = new Version(3, 0);
+    }
+}
