fedf141a44ba1a7b0a26bb1be73ada916da0cef5
Michael Simons <msimons@microsoft.com>
2018-11-29 13:47:26 -0600

    Update Dockerfiles to reference Windows images from MCR (#825)

--- scripts/Invoke-CleanupDocker.ps1
+++ scripts/Invoke-CleanupDocker.ps1
@@ -8,10 +8,7 @@ docker volume prune -f
 # Windows base images are large, preserve them to avoid the overhead of pulling each time.
 docker images |
     Where-Object {
-        -Not ($_.StartsWith("microsoft/nanoserver ")`
-        -Or $_.StartsWith("microsoft/windowsservercore ")`
-        -Or $_.StartsWith("mcr.microsoft.com/windows/nanoserver ")`
-        -Or $_.StartsWith("mcr.microsoft.com/windows/servercore ")`
+        -Not ($_.StartsWith("mcr.microsoft.com/windows")`
         -Or $_.StartsWith("REPOSITORY ")) } |
     ForEach-Object { $_.Split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)[2] } |
     Select-Object -Unique |
