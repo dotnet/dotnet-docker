26a2a01c9ea8620accfe6f09595d7757866d204c
Aditya Patwardhan <adityap@microsoft.com>
2019-03-15 14:27:34 -0700

    Move test location in file

--- tests/Microsoft.DotNet.Docker.Tests/ImageTests.cs
+++ tests/Microsoft.DotNet.Docker.Tests/ImageTests.cs
@@ -134,6 +134,27 @@ namespace Microsoft.DotNet.Docker.Tests
 
         [Theory]
         [MemberData(nameof(GetImageData))]
+        public void VerifySDKImage_PowerShellScenario(ImageData imageData)
+        {
+            if (imageData.Version.Major < 3)
+            {
+                _outputHelper.WriteLine("PowerShell does not exist in pre-3.0 images, skip testing");
+                return;
+            }
+
+            string output = _dockerHelper.Run(
+                image: imageData.GetImage(DotNetImageType.SDK, _dockerHelper),
+                name: imageData.GetIdentifier($"pwsh"),
+                command: $"pwsh -c get-date -Format 'yyyy:MM:dd'"
+            );
+
+            string expected = DateTime.Now.ToString("yyyy:MM:dd");
+
+            Assert.Equal(output, expected);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetImageData))]
         public async Task VerifyRuntimeImage_AppScenario(ImageData imageData)
         {
             ImageScenarioVerifier verifier = new ImageScenarioVerifier(imageData, _dockerHelper, _outputHelper);
@@ -152,27 +173,6 @@ namespace Microsoft.DotNet.Docker.Tests
 
             ImageScenarioVerifier verifier = new ImageScenarioVerifier(imageData, _dockerHelper, _outputHelper, isWeb: true);
             await verifier.Execute();
-        }
-
-        [Theory]
-        [MemberData(nameof(GetImageData))]
-        public void VerifySDKImage_PowerShellScenario(ImageData imageData)
-        {
-            if (imageData.Version.Major < 3)
-            {
-                _outputHelper.WriteLine("PowerShell does not exist in pre-3.0 images, skip testing");
-                return;
-            }
-
-            string output = _dockerHelper.Run(
-                image: imageData.GetImage(DotNetImageType.SDK, _dockerHelper),
-                name: imageData.GetIdentifier($"pwsh"),
-                command: $"pwsh -c get-date -Format 'yyyy:MM:dd'"
-            );
-
-            string expected = DateTime.Now.ToString("yyyy:MM:dd");
-
-            Assert.Equal(output, expected);
         }
     }
 }
