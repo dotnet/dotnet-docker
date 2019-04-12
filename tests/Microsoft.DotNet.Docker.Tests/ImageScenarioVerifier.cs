3f1e0e38ffefef96dca7cdcfa152cd9c02d81c1a
MichaelSimons <msimons@microsoft.com>
2019-04-12 13:26:07 -0500

    Add TODO comment to remove workaround

--- tests/Microsoft.DotNet.Docker.Tests/ImageScenarioVerifier.cs
+++ tests/Microsoft.DotNet.Docker.Tests/ImageScenarioVerifier.cs
@@ -132,6 +132,9 @@ namespace Microsoft.DotNet.Docker.Tests
                 ApplyProjectCustomizations(_imageData, Path.Combine(appDir, "app.csproj"));
 
                 string sourceDockerfileName = $"Dockerfile.{DockerHelper.DockerOS.ToLower()}";
+
+                // TODO: Remove Windows arm workaround once underlying Windows/Docker issue is resolved
+                // https://github.com/dotnet/dotnet-docker/issues/1054
                 if (!DockerHelper.IsLinuxContainerModeEnabled && _imageData.Arch == Arch.Arm)
                 {
                     sourceDockerfileName += $".{Enum.GetName(typeof(Arch), _imageData.Arch).ToLowerInvariant()}";
