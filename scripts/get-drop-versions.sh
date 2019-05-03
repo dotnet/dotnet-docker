8abe574ce067eecee91c251bc6bb184d7d910e3a
Michael Simons <msimons@microsoft.com>
2019-05-03 08:58:42 -0700

    Merge build infrastructure changes from nightly into master (#1101)
    * Add pipeline for updating .NET Core dependencies from drop location (#1090)
    
    * Introduce build pipeline stages (#1092)
    
    * Introduce build stages
    
    * Explicitly add component governance task (#1086)
    
    * Moving Component Detection to publish job so it only runs once
    
    * Enable PR validation for Arm32 images (#1068)

---
+++ scripts/get-drop-versions.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Stop script on NZEC
+set -e
+# Stop script if unbound variable found (use ${var:-} if intentional)
+set -u
+
+channel=$1
+
+curl -SLo sdk.zip https://dotnetcli.blob.core.windows.net/dotnet/Sdk/release/$channel/dotnet-sdk-latest-win-x64.zip
+unzip sdk.zip -d sdk
+rm sdk.zip
+
+sdkVersionsPath=$(find sdk/sdk -name .version)
+sdkVer=$(cat $sdkVersionsPath | head -2 | tail -1)
+
+runtimeVersionsPath=$(find sdk/shared/Microsoft.NETCore.App -name .version)
+runtimeVer=$(cat $runtimeVersionsPath | tail -1)
+
+aspnetVersionsPath=$(find sdk/shared/Microsoft.AspNetCore.App -name .version)
+aspnetVer=$(cat $aspnetVersionsPath | tail -1)
+
+rm -rf sdk
+
+echo "##vso[task.setvariable variable=sdkVer]$sdkVer"
+echo "##vso[task.setvariable variable=runtimeVer]$runtimeVer"
+echo "##vso[task.setvariable variable=aspnetVer]$aspnetVer"
