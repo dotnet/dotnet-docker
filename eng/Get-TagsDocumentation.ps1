b8e2ce6d90a732bfad72bdf2b307d973371c3de0
Matt Thalman <mthalman@microsoft.com>
2019-06-07 15:39:03 -0500

    Common engineering infrastructure folder

---
+++ eng/Get-TagsDocumentation.ps1
@@ -0,0 +1,26 @@
+#!/usr/bin/env pwsh
+param(
+    [string]$Branch
+)
+
+$ErrorActionPreference = 'Stop'
+$repoRoot = (Get-Item "$PSScriptRoot").Parent.FullName
+
+if (!$Branch) {
+    $manifestJson = Get-Content ${repoRoot}/manifest.json | ConvertFrom-Json
+    if ($manifestJson.Repos[0].Name.Contains("nightly")) {
+        $Branch = "nightly"
+        $coreRepoName = "core-nightly"
+    }
+    else {
+        $Branch = "master"
+        $coreRepoName = "core"
+    }
+}
+$gitRepo = "https://github.com/dotnet/dotnet-docker"
+
+& $PSScriptRoot/common/Invoke-ReadmeGeneration.ps1 dotnet/$coreRepoName/runtime-deps README.runtime-deps.md manifest.json $Branch $gitRepo
+& $PSScriptRoot/common/Invoke-ReadmeGeneration.ps1 dotnet/$coreRepoName/runtime README.runtime.md manifest.json $Branch $gitRepo -ReuseImageBuilderImage
+& $PSScriptRoot/common/Invoke-ReadmeGeneration.ps1 dotnet/$coreRepoName/aspnet README.aspnet.md manifest.json $Branch $gitRepo -ReuseImageBuilderImage
+& $PSScriptRoot/common/Invoke-ReadmeGeneration.ps1 dotnet/$coreRepoName/sdk README.sdk.md manifest.json $Branch $gitRepo -ReuseImageBuilderImage
+& $PSScriptRoot/common/Invoke-ReadmeGeneration.ps1 dotnet/core/samples README.samples.md manifest.samples.json master $gitRepo -ReuseImageBuilderImage
---
+++ eng/pipelines/README.md
@@ -0,0 +1,1 @@
+The contents of the `pipelines` folder are used by the .NET Core engineering infrastructure to build and publish the images.
