16512f5640ba3f33cc3b4f3c02eceafcfcac9218
Michael Simons <msimons@microsoft.com>
2019-08-06 13:09:18 -0500

    Update test project to use .NET Core 3.0 (#1251)

--- tests/run-tests.ps1
+++ tests/run-tests.ps1
@@ -41,10 +41,10 @@ if (!(Test-Path $DotnetInstallScript)) {
 
 if ($IsRunningOnUnix) {
     & chmod +x $DotnetInstallDir/$DotnetInstallScript
-    & $DotnetInstallDir/$DotnetInstallScript --channel "2.1" --version "latest" --architecture x64 --install-dir $DotnetInstallDir
+    & $DotnetInstallDir/$DotnetInstallScript --channel "release/3.0.1xx" --version "3.0.100-preview7-012821" --architecture x64 --install-dir $DotnetInstallDir
 }
 else {
-    & $DotnetInstallDir/$DotnetInstallScript -Channel "2.1" -Version "latest" -Architecture x64 -InstallDir $DotnetInstallDir
+    & $DotnetInstallDir/$DotnetInstallScript -Channel "release/3.0.1xx" -Version "3.0.100-preview7-012821" -Architecture x64 -InstallDir $DotnetInstallDir
 }
 
 if ($LASTEXITCODE -ne 0) { throw "Failed to install the .NET Core SDK" }
