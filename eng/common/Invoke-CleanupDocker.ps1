ca720d87cf371dfde03ddc69402dc0890b10d310
Michael Simons <msimons@microsoft.com>
2019-08-08 08:34:13 -0500

    Refactor Invoke-CleanupDocker.ps1 to preserve the eng infra images to avoid re-pulls (#1259)

--- eng/common/Invoke-CleanupDocker.ps1
+++ eng/common/Invoke-CleanupDocker.ps1
@@ -5,11 +5,17 @@ docker ps -a -q | ForEach-Object { docker rm -f $_ }
 
 docker volume prune -f
 
-# Windows base images are large, preserve them to avoid the overhead of pulling each time.
-docker images |
+# Preserve the Windows base images and the common eng infra images (e.g. ImageBuilder)
+# to avoid the expense of having to repull continuously.
+$engInfraImages = Get-Content ./eng/common/templates/variables/docker-images.yml |
+    Where-Object { $_.Trim() -notlike 'variables:' } |
+    ForEach-Object { $_.Split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)[1] }
+
+docker images --format "{{.Repository}}:{{.Tag}} {{.ID}}"|
     Where-Object {
-        -Not ($_.StartsWith("mcr.microsoft.com/windows")`
-        -Or $_.StartsWith("REPOSITORY ")) } |
-    ForEach-Object { $_.Split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)[2] } |
+        $localImage = $_
+        -Not ($localImage.StartsWith("mcr.microsoft.com/windows")`
+            -Or ($engInfraImages.Where({ $localImage.StartsWith($_) }, 'First').Count -gt 0)) } |
+    ForEach-Object { $_.Split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)[1] } |
     Select-Object -Unique |
     ForEach-Object { docker rmi -f $_ }
