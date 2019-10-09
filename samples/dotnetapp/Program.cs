2134acab79b0b3ce13b05d8b161fa7fb79474986
Rich Lander <rlander@microsoft.com>
2019-10-09 15:51:52 -0700

    Simplify sample

--- samples/dotnetapp/Program.cs
+++ samples/dotnetapp/Program.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Runtime.InteropServices;
-using System.IO;
-using System.Text;
-using Utils;
 using static System.Console;
 
 public static class Program
@@ -10,41 +7,14 @@ public static class Program
     public static void Main(string[] args) 
     {
           var defaultMessage = "Hello from .NET Core!";
-          var bot = GetBot();
-          var (message, withColor) = ParseArgs(args);
-          var output = message == string.Empty ? $"    {defaultMessage}{bot}" : $"    {message}{bot}";
+          var message = args is object && args.Length > 0 ? string.Join(' ',args): defaultMessage ;
 
-          if (withColor)
-          {
-              ConsoleUtils.PrintStringWithRandomColor(output);
-          }
-          else
-          {
-              WriteLine(output);
-          }
-          
-          WriteLine("**Environment**");
-          WriteLine($"Platform: .NET Core");
-          WriteLine($"OS: {RuntimeInformation.OSDescription}");
           WriteLine();
-    }
-
-    private static (string, bool) ParseArgs(string[] args)
-    {
-      var buffer = new StringBuilder();
-      var withColor = false;
-      foreach(var s in args)
-      {
-        if (s == "--with-color")
-        {
-          withColor = true;
-          continue;
-        }
-        buffer.Append(" ");
-        buffer.Append(s);
-      }
-
-      return (buffer.ToString(), withColor);
+          WriteLine($"      {message}{GetBot()}");
+          
+          WriteLine("Environment:");
+          WriteLine(RuntimeInformation.FrameworkDescription);
+          WriteLine(RuntimeInformation.OSDescription);
     }
 
     private static string GetBot() 
@@ -88,7 +58,6 @@ public static class Program
         ...............                 .'..............
         .........                        ..............
           .....
-
   ";
     }
 }
