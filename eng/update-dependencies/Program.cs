d8b43b6ba32d6d171154b353ccd2a0f94d9bc47d
Matt Thalman <mthalman@microsoft.com>
2019-12-17 14:02:44 -0600

    Prevent force-pushes on PR in update-dependencies (#1533)

--- eng/update-dependencies/Program.cs
+++ eng/update-dependencies/Program.cs
@@ -1,20 +1,21 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.DotNet.VersionTools;
-using Microsoft.DotNet.VersionTools.Automation;
-using Microsoft.DotNet.VersionTools.BuildManifest.Model;
-using Microsoft.DotNet.VersionTools.Dependencies;
-using Microsoft.DotNet.VersionTools.Dependencies.BuildOutput;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using LibGit2Sharp;
+using Microsoft.DotNet.VersionTools;
+using Microsoft.DotNet.VersionTools.Automation;
+using Microsoft.DotNet.VersionTools.Automation.GitHubApi;
+using Microsoft.DotNet.VersionTools.BuildManifest.Model;
+using Microsoft.DotNet.VersionTools.Dependencies;
+using Microsoft.DotNet.VersionTools.Dependencies.BuildOutput;
 
 namespace Dotnet.Docker
 {
@@ -45,7 +46,7 @@ namespace Dotnet.Docker
                     }
                     else
                     {
-                        await CreatePullRequestAsync(buildInfos);
+                        await CreatePullRequestAsync();
                     }
                 }
             }
@@ -147,7 +148,7 @@ namespace Dotnet.Docker
                 Enumerable.Empty<string>());
         }
 
-        private static async Task CreatePullRequestAsync(IEnumerable<IDependencyInfo> buildInfos)
+        private static async Task CreatePullRequestAsync()
         {
             GitHubAuth gitHubAuth = new GitHubAuth(Options.GitHubPassword, Options.GitHubUser, Options.GitHubEmail);
             PullRequestCreator prCreator = new PullRequestCreator(gitHubAuth, Options.GitHubUser);
@@ -157,14 +158,159 @@ namespace Dotnet.Docker
             };
 
             string commitMessage = $"[{Options.GitHubUpstreamBranch}] Update dependencies from dotnet/core-sdk";
+            GitHubProject upstreamProject = new GitHubProject(Options.GitHubProject, Options.GitHubUpstreamOwner);
+            GitHubBranch upstreamBranch = new GitHubBranch(Options.GitHubUpstreamBranch, upstreamProject);
 
-            await prCreator.CreateOrUpdateAsync(
-                commitMessage,
-                commitMessage,
-                string.Empty,
-                new GitHubBranch(Options.GitHubUpstreamBranch, new GitHubProject(Options.GitHubProject, Options.GitHubUpstreamOwner)),
-                new GitHubProject(Options.GitHubProject, gitHubAuth.User),
-                prOptions);
+            using (GitHubClient client = new GitHubClient(gitHubAuth))
+            {
+                GitHubPullRequest pullRequestToUpdate = await client.SearchPullRequestsAsync(
+                    upstreamProject,
+                    upstreamBranch.Name,
+                    await client.GetMyAuthorIdAsync());
+
+                if (pullRequestToUpdate == null)
+                {
+                    await prCreator.CreateOrUpdateAsync(
+                        commitMessage,
+                        commitMessage,
+                        string.Empty,
+                        upstreamBranch,
+                        new GitHubProject(Options.GitHubProject, gitHubAuth.User),
+                        prOptions);
+                }
+                else
+                {
+                    UpdateExistingPullRequest(gitHubAuth, prOptions, commitMessage, upstreamBranch);
+                }
+            }
+        }
+
+        private static void UpdateExistingPullRequest(
+            GitHubAuth gitHubAuth, PullRequestOptions prOptions, string commitMessage, GitHubBranch upstreamBranch)
+        {
+            // PullRequestCreator ends up force-pushing updates to an existing PR which is not great when the logic
+            // gets called on a schedule (see https://github.com/dotnet/dotnet-docker/issues/1114). To avoid this,
+            // it needs the ability to only update files that have changed from the existing PR.  Because the
+            // PullRequestCreator class doesn't rely on there being a locally cloned repo, it doesn't have an
+            // efficient way to determine whether files have changed or not. Update-dependencies would have to
+            // implement logic which pulls down each file individually from the API and compare it to what exists
+            // in the local repo.  Since that's not an efficient process, this method works by cloning the PR's
+            // branch to a temporary repo location, grabbing the whole repo where the original updates from
+            // update-dependencies were made and copying it into the temp repo, and committing and pushing 
+            // those changes in the temp repo back to the PR's branch.
+
+            string tempRepoPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+
+            try
+            {
+                string branchName = prOptions.BranchNamingStrategy.Prefix(upstreamBranch.Name);
+                CloneOptions cloneOptions = new CloneOptions
+                {
+                    BranchName = branchName
+                };
+
+                // Clone the PR's repo/branch to a temp location
+                Repository.Clone($"https://github.com/{gitHubAuth.User}/{Options.GitHubProject}", tempRepoPath, cloneOptions);
+
+                // Remove all existing directories and files from the temp repo
+                ClearRepoContents(tempRepoPath);
+
+                // Copy contents of local repo changes to temp repo
+                DirectoryCopy(".", tempRepoPath);
+
+                using Repository repo = new Repository(tempRepoPath);
+                RepositoryStatus status = repo.RetrieveStatus(new StatusOptions());
+
+                // If there are any changes from what exists in the PR
+                if (status.IsDirty)
+                {
+                    Commands.Stage(repo, "*");
+
+                    Signature signature = new Signature(Options.GitHubUser, Options.GitHubEmail, DateTimeOffset.Now);
+                    repo.Commit(commitMessage, signature, signature);
+
+                    Branch branch = repo.Branches[$"origin/{branchName}"];
+
+                    PushOptions pushOptions = new PushOptions
+                    {
+                        CredentialsProvider = (url, user, credTypes) => new UsernamePasswordCredentials
+                        {
+                            Username = Options.GitHubPassword,
+                            Password = String.Empty
+                        }
+                    };
+
+                    Remote remote = repo.Network.Remotes["origin"];
+                    string pushRefSpec = $@"refs/heads/{branchName}";
+
+                    repo.Network.Push(remote, pushRefSpec, pushOptions);
+                }
+            }
+            finally
+            {
+                // Cleanup temp repo
+                DeleteRepoDirectory(tempRepoPath);
+            }
+        }
+
+        private static void DeleteRepoDirectory(string repoPath)
+        {
+            if (Directory.Exists(repoPath))
+            {
+                IEnumerable<string> gitFiles = Directory.GetFiles(
+                    Path.Combine(repoPath, ".git"), "*", SearchOption.AllDirectories);
+
+                // Ensure all files in .git folder are writable
+                foreach (string file in gitFiles)
+                {
+                    File.SetAttributes(file, FileAttributes.Normal);
+                }
+
+                Directory.Delete(repoPath, true);
+            }
+        }
+
+        private static void ClearRepoContents(string repoPath)
+        {
+            foreach (string file in Directory.GetFiles(repoPath))
+            {
+                File.Delete(file);
+            }
+            foreach (DirectoryInfo dir in new DirectoryInfo(repoPath).GetDirectories().Where(dir => dir.Name != ".git"))
+            {
+                Directory.Delete(dir.FullName, true);
+            }
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            DirectoryInfo[] dirs = dir.GetDirectories()
+                .Where(dir => !dir.Name.StartsWith("."))
+                .ToArray();
+
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            foreach (DirectoryInfo subdir in dirs)
+            {
+                string temppath = Path.Combine(destDirName, subdir.Name);
+                DirectoryCopy(subdir.FullName, temppath);
+            }
         }
 
         private static string GetBuildVersion(this IEnumerable<IDependencyInfo> buildInfos, string name)
