4066c437cc20b689c4dc6f0781bd2171d7aedc40
Rich Lander <rlander@microsoft.com>
2020-01-11 11:18:24 -0800

    Update script name

--- samples/test-samples.ps1
+++ samples/test-samples.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 
-./build-images.ps1 dotnetapp 1
-./build-images.ps1 ./complexapp 1
-./build-images.ps1 ./aspnetapp
+./build-test-images.ps1 dotnetapp 1
+./build-test-images.ps1 ./complexapp 1
+./build-test-images.ps1 ./aspnetapp
 ./complexapp/build-and-test.ps1 -path ./complexapp
