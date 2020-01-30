687329185faedabb6c05ce24a85aa4736a958721
Matt Thalman <mthalman@microsoft.com>
2020-01-30 15:48:33 -0600

    Ensure update-dependencies fails if sha not found (#1644)

--- eng/update-dependencies/Program.cs
+++ eng/update-dependencies/Program.cs
@@ -30,6 +30,8 @@ namespace Dotnet.Docker
         {
             try
             {
+                ErrorTraceListener errorTraceListener = new ErrorTraceListener();
+                Trace.Listeners.Add(errorTraceListener);
                 Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
 
                 Options.Parse(args);
@@ -46,6 +48,14 @@ namespace Dotnet.Docker
                     {
                         await CreatePullRequestAsync();
                     }
+                }
+
+                if (errorTraceListener.Errors.Any())
+                {
+                    string errors = String.Join(Environment.NewLine, errorTraceListener.Errors);
+                    Console.Error.WriteLine(
+                        $"Failed to update dependencies due to the following errors:{Environment.NewLine}{errors}");
+                    Environment.Exit(1);
                 }
             }
             catch (Exception e)
