300f6216c8bffaee08d8342fcd57b28081b16269
Matt Thalman <mthalman@microsoft.com>
2020-03-16 12:44:20 -0500

    Remove testing for 5.0 Focal ARM32

--- tests/Microsoft.DotNet.Docker.Tests/TestData.cs
+++ tests/Microsoft.DotNet.Docker.Tests/TestData.cs
@@ -34,7 +34,6 @@ namespace Microsoft.DotNet.Docker.Tests
             new ProductImageData { Version = V5_0, OS = OS.Focal,        Arch = Arch.Amd64 },
             new ProductImageData { Version = V5_0, OS = OS.Alpine311,    Arch = Arch.Amd64 },
             new ProductImageData { Version = V5_0, OS = OS.BusterSlim,   Arch = Arch.Arm },
-            new ProductImageData { Version = V5_0, OS = OS.Focal,        Arch = Arch.Arm },
             new ProductImageData { Version = V5_0, OS = OS.BusterSlim,   Arch = Arch.Arm64 },
             new ProductImageData { Version = V5_0, OS = OS.Focal,        Arch = Arch.Arm64 },
             new ProductImageData { Version = V5_0, OS = OS.Alpine311,    Arch = Arch.Arm64,   SdkOS = OS.Buster },
