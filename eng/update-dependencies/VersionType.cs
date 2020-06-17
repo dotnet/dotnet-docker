4a3bce5568b50023eb365c955a2ff7aad9858548
Michael Simons <msimons@microsoft.com>
2020-06-17 10:56:48 -0500

    Add Dockerfile templates

---
+++ eng/update-dependencies/VersionType.cs
@@ -0,0 +1,14 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Dotnet.Docker
+{
+    /// <summary>
+    /// The different type of version numbers.
+    /// </summary>
+    public enum VersionType
+    {
+        Build,
+        Product
+    }
+}
