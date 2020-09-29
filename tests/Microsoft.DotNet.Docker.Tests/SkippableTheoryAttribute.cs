ea8f4a1a8bfaeba6a1c1c37cdad4517827036864
Matt Thalman <mthalman@microsoft.com>
2020-09-29 10:20:32 -0500

    Windows Server Core 2019 Dockerfiles for 5.0 (#2274)

---
+++ tests/Microsoft.DotNet.Docker.Tests/SkippableTheoryAttribute.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace Microsoft.DotNet.Docker.Tests
+{
+    public class SkippableTheoryAttribute : TheoryAttribute
+    {
+        public SkippableTheoryAttribute(params string[] skippedOperatingSystems)
+        {
+            if (!string.IsNullOrEmpty(Config.Os) && Config.Os != "*")
+            {
+                string osPattern =
+                    Config.Os != null ? Config.GetFilterRegexPattern(Config.Os) : null;
+                foreach (string skippedOperatingSystem in skippedOperatingSystems)
+                {
+                    if (Regex.IsMatch(skippedOperatingSystem, osPattern, RegexOptions.IgnoreCase))
+                    {
+                        Skip = $"{skippedOperatingSystem} is unsupported";
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
