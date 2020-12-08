b23064152970d1fc7545f380942b0d697c8cf619
Justin Anderson <jander-msft@users.noreply.github.com>
2020-12-08 05:49:25 -0800

    Update dotnet-monitor version detection and pull package from Azure Artifacts feed. (#2436)

--- eng/get-drop-versions-monitor.sh
+++ eng/get-drop-versions-monitor.sh
@@ -5,14 +5,11 @@ set -e
 # Stop script if unbound variable found (use ${var:-} if intentional)
 set -u
 
-channel=$1
+curl -SLo dotnet-monitor.nupkg.version https://aka.ms/dotnet/diagnostics/monitor5.0/dotnet-monitor.nupkg.version
 
-curl -SLo dotnet-monitor.nupkg https://aka.ms/dotnet/$channel/diagnostics/monitor5.0/dotnet-monitor.nupkg
-# In nuspec, there is only one element named "version" and it is the version of the nupkg.
-# All other uses of "version" are attributes on other elements. grep using Perl regex, reporting
-# the first match, and only printing what was matched.
-monitorVer=$(unzip -p dotnet-monitor.nupkg dotnet-monitor.nuspec | cat | grep -oPm1 "(?<=<version>)[^<]+")
+# Read version file and remove newlines
+monitorVer=$(tr -d '\r\n' < dotnet-monitor.nupkg.version)
 
-rm dotnet-monitor.nupkg
+rm dotnet-monitor.nupkg.version
 
 echo "##vso[task.setvariable variable=monitorVer]$monitorVer"
