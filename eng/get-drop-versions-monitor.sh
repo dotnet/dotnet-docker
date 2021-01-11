60607a31d0c2021ff6613c10aded6548dd4b5987
Matt Thalman <mthalman@microsoft.com>
2021-01-11 09:26:05 -0600

    Update dotnet-monitor version detection and pull package from Azure Artifacts feed. (#2436)

---
+++ eng/get-drop-versions-monitor.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Stop script on NZEC
+set -e
+# Stop script if unbound variable found (use ${var:-} if intentional)
+set -u
+
+curl -SLo dotnet-monitor.nupkg.version https://aka.ms/dotnet/diagnostics/monitor5.0/dotnet-monitor.nupkg.version
+
+# Read version file and remove newlines
+monitorVer=$(tr -d '\r\n' < dotnet-monitor.nupkg.version)
+
+rm dotnet-monitor.nupkg.version
+
+echo "##vso[task.setvariable variable=monitorVer]$monitorVer"
