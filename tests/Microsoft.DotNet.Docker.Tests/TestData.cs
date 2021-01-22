816bccf4ed509897ce422343018b1231b367c8c7
Matt Thalman <mthalman@microsoft.com>
2021-01-22 10:10:43 -0600

    Add test leg for 5.0 Alpine Arm32

--- tests/Microsoft.DotNet.Docker.Tests/TestData.cs
+++ tests/Microsoft.DotNet.Docker.Tests/TestData.cs
@@ -45,6 +45,7 @@ namespace Microsoft.DotNet.Docker.Tests
             new ProductImageData { Version = V5_0, OS = OS.Focal,        Arch = Arch.Arm64 },
             new ProductImageData { Version = V5_0, OS = OS.Alpine312,    Arch = Arch.Arm,     SdkOS = OS.BusterSlim },
             new ProductImageData { Version = V5_0, OS = OS.Alpine312,    Arch = Arch.Arm64,   SdkOS = OS.BusterSlim },
+            new ProductImageData { Version = V5_0, OS = OS.Alpine313,    Arch = Arch.Arm,     SdkOS = OS.BusterSlim },
             new ProductImageData { Version = V5_0, OS = OS.Alpine313,    Arch = Arch.Arm64,   SdkOS = OS.BusterSlim },
             new ProductImageData { Version = V6_0, OS = OS.BusterSlim,   Arch = Arch.Amd64 },
             new ProductImageData { Version = V6_0, OS = OS.Focal,        Arch = Arch.Amd64 },
