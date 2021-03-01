9855345548b2e14a6eb8167d27bd1f069936fe0f
Justin Anderson <jander-msft@users.noreply.github.com>
2021-03-01 09:00:22 -0800

    Update dotnet-monitor latest version URL. (#2628)

--- eng/get-drop-versions-monitor.sh
+++ eng/get-drop-versions-monitor.sh
@@ -5,7 +5,7 @@ set -e
 # Stop script if unbound variable found (use ${var:-} if intentional)
 set -u
 
-curl -SLo dotnet-monitor.nupkg.version https://aka.ms/dotnet/diagnostics/monitor5.0/dotnet-monitor.nupkg.version
+curl -SLo dotnet-monitor.nupkg.version https://aka.ms/dotnet/dotnet-monitor.nupkg.version
 
 # Read version file and remove newlines
 monitorVer=$(tr -d '\r\n' < dotnet-monitor.nupkg.version)
