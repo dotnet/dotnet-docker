c0d0ee41932ff30f1eb2e9a1dd0faf92b3dceb9c
Rich Lander <rlander@microsoft.com>
2021-03-24 09:31:59 -0700

    Update samples (#2694)
    * Update samples
    
    * Match SDK and runtime stages
    
    * Update Nanoserver to 1809
    
    * Simplify usings
    
    * A logic to break of out loop
    
    * Update image test
    
    * Update test to cover old and new cases
    
    * Update Nano Server from tags
    
    * Add human-readable memory units
    
    * Correct gibi size
    
    * Update per feedback
    
    * Update per feedback
    
    * Add newline at EOF
    
    * Apply suggestions from code review
    
    Co-authored-by: Matt Thalman <mthalman@microsoft.com>
    
    * Update per feedback
    
    Co-authored-by: Matt Thalman <mthalman@microsoft.com>

--- samples/dotnetapp/Program.cs
+++ samples/dotnetapp/Program.cs
@@ -1,64 +1,97 @@
 using System;
+using System.IO;
 using System.Runtime.InteropServices;
 using static System.Console;
 
-public static class Program
+// Variant of https://github.com/dotnet/core/tree/main/samples/dotnet-runtimeinfo
+// Ascii text: https://ascii.co.uk/text (Univers font)
+
+string nl = Environment.NewLine;
+
+WriteLine(
+$"         42{nl}" +
+$"         42              ,d                             ,d{nl}" +
+$"         42              42                             42{nl}" +
+$" ,adPPYb,42  ,adPPYba, MM42MMM 8b,dPPYba,   ,adPPYba, MM42MMM{nl}" +
+$"a8\"    `Y42 a8\"     \"8a  42    42P\'   `\"8a a8P_____42   42{nl}" +
+$"8b       42 8b       d8  42    42       42 8PP\"\"\"\"\"\"\"   42{nl}" +
+$"\"8a,   ,d42 \"8a,   ,a8\"  42,   42       42 \"8b,   ,aa   42,{nl}" +
+$" `\"8bbdP\"Y8  `\"YbbdP\"\'   \"Y428 42       42  `\"Ybbd8\"\'   \"Y428{nl}");
+
+
+// .NET information
+WriteLine(RuntimeInformation.FrameworkDescription);
+
+// OS information
+const string OSRel = "/etc/os-release";
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && 
+    File.Exists(OSRel))
 {
-    public static void Main(string[] args) 
+  const string PrettyName = "PRETTY_NAME";
+  foreach(string line in File.ReadAllLines(OSRel))
+  {
+      if (line.StartsWith(PrettyName))
+      {
+          ReadOnlySpan<char> value = line.AsSpan()[(PrettyName.Length + 2)..^1];
+          WriteLine(value.ToString());
+          break;
+      }
+  }
+}
+else
+{
+    WriteLine(RuntimeInformation.OSDescription);
+}
+
+WriteLine();
+
+// Environment information
+WriteLine($"{nameof(RuntimeInformation.OSArchitecture)}: {RuntimeInformation.OSArchitecture}");
+WriteLine($"{nameof(Environment.ProcessorCount)}: {Environment.ProcessorCount}");
+
+const long Mebi = 1024 * 1024;
+const long Gibi = Mebi * 1024;
+
+// cgroup information
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && 
+    Directory.Exists("/sys/fs/cgroup/cpu") &&
+    Directory.Exists("/sys/fs/cgroup/memory"))
+{
+    // get cpu cgroup information
+    string cpuquota = File.ReadAllLines("/sys/fs/cgroup/cpu/cpu.cfs_quota_us")[0];
+    if (int.TryParse(cpuquota, out int quota) &&
+        quota > 0)
     {
-          var defaultMessage = "Hello from .NET!";
-          var message = args is object && args.Length > 0 ? string.Join(' ', args): defaultMessage;
-
-          WriteLine();
-          WriteLine($"      {message}{GetBot()}");
-          
-          WriteLine("Environment:");
-          WriteLine($"{RuntimeInformation.FrameworkDescription} ({RuntimeInformation.ProcessArchitecture})");
-          WriteLine(RuntimeInformation.OSDescription);
-
+        WriteLine($"cfs_quota_us: {quota}");
     }
 
-    private static string GetBot() 
+    // get memory cgroup information
+    string usageBytes = File.ReadAllLines("/sys/fs/cgroup/memory/memory.usage_in_bytes")[0];
+    string limitBytes = File.ReadAllLines("/sys/fs/cgroup/memory/memory.limit_in_bytes")[0];
+    if (long.TryParse(usageBytes, out long usage) &&
+        long.TryParse(limitBytes, out long limit) &&
+        // above this size is unlikely to be an intentionally constrained cgroup
+        limit < 10 * Gibi)
     {
-            
-            return @"
-      __________________
-                        \
-                        \
-                            ....
-                            ....'
-                            ....
-                          ..........
-                      .............'..'..
-                  ................'..'.....
-                .......'..........'..'..'....
-                ........'..........'..'..'.....
-              .'....'..'..........'..'.......'.
-              .'..................'...   ......
-              .  ......'.........         .....
-              .                           ......
-              ..    .            ..        ......
-            ....       .                 .......
-            ......  .......          ............
-              ................  ......................
-              ........................'................
-            ......................'..'......    .......
-          .........................'..'.....       .......
-      ........    ..'.............'..'....      ..........
-    ..'..'...      ...............'.......      ..........
-    ...'......     ...... ..........  ......         .......
-  ...........   .......              ........        ......
-  .......        '...'.'.              '.'.'.'         ....
-  .......       .....'..               ..'.....
-    ..       ..........               ..'........
-            ............               ..............
-          .............               '..............
-          ...........'..              .'.'............
-        ...............              .'.'.............
-        .............'..               ..'..'...........
-        ...............                 .'..............
-        .........                        ..............
-          .....
-  ";
+        WriteLine($"usage_in_bytes: {usageBytes} {GetInBiggerUnit(usage)}");
+        WriteLine($"limit_in_bytes: {limitBytes} {GetInBiggerUnit(limit)}");
+    }
+}
+
+string GetInBiggerUnit(long size)
+{
+    if (size < Mebi)
+    {
+        return string.Empty;
+    }
+    else if (size < Gibi)
+    {
+        long mebibytes = size / Mebi;
+        return $"({mebibytes} MiB)";
+    }
+    else
+    {
+        long gibibytes = size / Gibi;
+        return $"({gibibytes} GiB)";
     }
 }
