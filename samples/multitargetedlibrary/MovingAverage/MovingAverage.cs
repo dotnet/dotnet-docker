b69f828711aac263b13d9d90adfbd868b95c3e99
Rich Lander <rlander@microsoft.com>
2021-03-31 21:50:03 -0700

    Add multitargeted tests

---
+++ samples/multitargetedlibrary/MovingAverage/MovingAverage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utils
+{
+    public class MovingAverage
+    {
+        private const int MinWindowsSize = 6;
+        private int _windowSize;
+        private int[] _values;
+        private int _average = 0;
+        private int _index = 0;
+        private int _sum = 0;
+
+        public MovingAverage(int windowSize = MinWindowsSize)
+        {
+            if (windowSize < MinWindowsSize)
+            {
+                throw new ArgumentException($"Window size must be {MinWindowsSize} or greater");
+            }
+
+            _windowSize = windowSize;
+            _values = new int[windowSize];
+        }
+
+        public int Window => _windowSize;
+
+        public int Add(int value)
+        {
+            if (_index >= _windowSize)
+            {
+                _index = 0;
+            }
+
+            _sum -= _values[_index];
+            _sum += value;
+            _values[_index++] = value;
+            _average = _sum / _windowSize;
+            return _average;
+        }
+
+        public int GetAverage() => _average;
+
+#if NETSTANDARD2_1
+        public async IAsyncEnumerable<int> GetAveragesAsync(IAsyncEnumerable<int> source)
+        {
+            await foreach (int result in source)
+            {
+                yield return Add(result);
+            }
+        }
+#endif
+    }
+}
---
+++ samples/multitargetedlibrary/tests/MovingAverageTests.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Xunit;
+using Utils;
+
+namespace tests
+{
+    public class MovingAverageTests
+    {
+        [Fact]
+        public void Average()
+        {
+            int[] values = Enumerable.Range(1, 10).ToArray();
+            MovingAverage avg = new(values.Length);
+            for (int i = 0; i < values.Length; i++)
+            {
+                avg.Add(values[i]);
+            }
+
+            Assert.True(avg.GetAverage() == 5);
+        }
+
+#if NET5_0
+        [Fact]
+        public async Task StreamingAverage()
+        {
+            //setup
+            int count = 100;
+            _sensorReadings = Enumerable.Range(1, count).ToArray();
+            MovingAverage avg = new(10);
+
+            await foreach(int average in avg.GetAveragesAsync(ReadSensorAsync(count)))
+            {
+                // could do something here
+            }
+
+            Assert.True(avg.GetAverage() == 95);
+        }
+
+        private int[] _sensorReadings;
+
+        private async IAsyncEnumerable<int> ReadSensorAsync(int readings)
+        {
+            int count = 0;
+            while (count < readings)
+            {
+                // perform reading
+                await Task.Delay(1);
+                yield return _sensorReadings[count++];
+            }
+            
+        }
+#endif
+    }
+}
