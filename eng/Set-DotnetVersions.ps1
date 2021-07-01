4764cd65666a70df027b330c413b691790dd6950
Matt Thalman <mthalman@microsoft.com>
2021-07-01 07:36:50 -0500

    Updates from review

--- eng/Set-DotnetVersions.ps1
+++ eng/Set-DotnetVersions.ps1
@@ -39,7 +39,7 @@ if ($SdkVersion) {
 }
 
 if ($AspnetVersion) {
-    $updateDepsArgs += @("--product-version", "aspnet=$AspnetVersion", "--product-version", "aspnet-runtime-targeting-pack=$AspnetVersion", "--product-version")
+    $updateDepsArgs += @("--product-version", "aspnet=$AspnetVersion", "--product-version", "aspnet-runtime-targeting-pack=$AspnetVersion")
 }
 
 if ($RuntimeVersion) {
