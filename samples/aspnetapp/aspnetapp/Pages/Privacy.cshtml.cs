ccd1df35ca6d04acb1be7cac33406a7800b630dc
Matt Thalman <mthalman@microsoft.com>
2021-09-02 13:42:00 -0500

    Update samples to .NET 6

--- samples/aspnetapp/aspnetapp/Pages/Privacy.cshtml.cs
+++ samples/aspnetapp/aspnetapp/Pages/Privacy.cshtml.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
-namespace aspnetapp.Pages
+namespace aspnetapp.Pages;
+public class PrivacyModel : PageModel
 {
-    public class PrivacyModel : PageModel
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
     {
-        private readonly ILogger<PrivacyModel> _logger;
+        _logger = logger;
+    }
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
+    public void OnGet()
+    {
     }
 }
+
