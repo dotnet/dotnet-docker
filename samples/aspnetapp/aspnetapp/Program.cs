99189a20a94bcd84ad04790137bddbf3feb972e0
Matt Thalman <mthalman@microsoft.com>
2021-09-15 09:31:57 -0500

    Update to use template from latest 6.0 RC1 release

--- samples/aspnetapp/aspnetapp/Program.cs
+++ samples/aspnetapp/aspnetapp/Program.cs
@@ -6,11 +6,7 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
