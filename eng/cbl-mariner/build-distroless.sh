7475b7eb4f69d906776f28ee87c4fe4c35f923f0
Matt Thalman <mthalman@microsoft.com>
2021-09-29 12:04:52 -0500

    Rewrite

--- eng/cbl-mariner/build-distroless.sh
+++ eng/cbl-mariner/build-distroless.sh
@@ -1,30 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -e
 
 marinerRepoTag="$1"
 outputPath="$2"
-scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-prereqsTag="mcr.microsoft.com/dotnet-buildtools/prereqs:ubuntu-20.04-cbl-mariner-20210924193732-ba78c9e"
-
-# Update Docker with new configuration
-# TODO: Can be removed when https://github.com/dotnet/dotnet-docker/issues/3142 is completed
-sudo systemctl stop docker
-sudo cp $scriptDir/daemon.json /etc/docker/daemon.json
-sudo systemctl start docker
+script=$(readlink -f "$0")
+scriptDir=$(dirname "$script")
 
 # Run container to build distroless tarball
-docker pull $prereqsTag
+docker build --pull -t mariner-prereqs $scriptDir
 containerOutputPath="/dotnet-runtime-deps.tar.gz"
-docker run --name mariner-build -v $scriptDir:/scripts --privileged $prereqsTag /scripts/toolkit-build.sh $marinerRepoTag $containerOutputPath
+docker run --name mariner-build --privileged mariner-prereqs ./toolkit-build.sh $marinerRepoTag $containerOutputPath
 
 # Copy tarball out of container and clean up
 docker cp mariner-build:$containerOutputPath $outputPath
 docker rm mariner-build
-
-# Restore Docker with original configuration
-# TODO: Can be removed when https://github.com/dotnet/dotnet-docker/issues/3142 is completed
-sudo systemctl stop docker
-sudo rm -rf /var/lib/docker/devicemapper
-sudo rm /etc/docker/daemon.json
-sudo systemctl start docker
--- eng/cbl-mariner/toolkit-build.sh
+++ eng/cbl-mariner/toolkit-build.sh
@@ -1,26 +1,26 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -e
 
 marinerRepoTag="$1"
 outputPath="$2"
+script=$(readlink -f "$0")
+scriptDir=$(dirname "$script")
 
 # Necessary to avoid having the toolkit think it's running in a CDPx build environment which would cause this build to not work
 rm /.dockerenv
 
-git clone https://github.com/microsoft/CBL-Mariner.git
-pushd /CBL-Mariner/toolkit
-git checkout $marinerRepoTag
+git clone --depth 1 --branch 1.0-stable https://github.com/microsoft/CBL-Mariner.git /CBL-Mariner
+cd /CBL-Mariner/toolkit
 make package-toolkit REBUILD_TOOLS=y
-popd
 
-# Set up a separate build location from the main repo
+# Set up a separate build location outside of the repo
 mkdir -p /builder/SPECS
-pushd /builder
-cp ../CBL-Mariner/out/toolkit-*.tar.gz ./
+cd /builder
+cp /CBL-Mariner/out/toolkit-*.tar.gz ./
 tar -xzvf toolkit-*.tar.gz
 cd toolkit
 
-make image CONFIG_FILE=/scripts/imageconfig.json
+make image CONFIG_FILE=$scriptDir/imageconfig.json
 
 mv /builder/out/images/imageconfig/dotnet-runtime-deps-*.tar.gz $outputPath
