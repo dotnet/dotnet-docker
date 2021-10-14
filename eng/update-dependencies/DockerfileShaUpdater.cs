e20882601fa33c9bca5e3ad87821e7c19f07cb72
Matt Thalman <mthalman@microsoft.com>
2021-10-14 13:20:23 -0500

    Update pipelines to conform to new infra

--- eng/update-dependencies/DockerfileShaUpdater.cs
+++ eng/update-dependencies/DockerfileShaUpdater.cs
@@ -91,7 +91,7 @@ namespace Dotnet.Docker
         public static IEnumerable<IDependencyUpdater> CreateUpdaters(
             string productName, string dockerfileVersion, string repoRoot, Options options)
         {
-            string versionsPath = System.IO.Path.Combine(repoRoot, Program.VersionsFilename);
+            string versionsPath = System.IO.Path.Combine(repoRoot, UpdateDependencies.VersionsFilename);
             string versions = File.ReadAllText(versionsPath);
 
             // The format of the sha variable name is '<productName>|<dockerfileVersion>|<os>|<arch>|sha'.
@@ -146,14 +146,8 @@ namespace Dotnet.Docker
 
             usedBuildInfos = new IDependencyInfo[] { productInfo };
 
-            const string RtmSubstring = "-rtm.";
-
             string? versionDir = _buildVersion;
-            string? versionFile = _buildVersion;
-            if (versionFile?.Contains(RtmSubstring) == true)
-            {
-                versionFile = versionFile.Substring(0, versionFile.IndexOf(RtmSubstring));
-            }
+            string? versionFile = UpdateDependencies.ResolveProductVersion(_buildVersion, _options);
 
             string archiveExt;
             if (_os.Contains("win"))
@@ -437,12 +431,22 @@ namespace Dotnet.Docker
 
         private static string? GetBuildVersion(string productName, string dockerfileVersion, string variables, Options options)
         {
+            string? buildVersion;
             if (options.ProductVersions.TryGetValue(productName, out string? version))
             {
-                return version;
+                buildVersion = version;
+            }
+            else
+            {
+                buildVersion = VersionUpdater.GetBuildVersion(productName, dockerfileVersion, variables);
             }
 
-            return VersionUpdater.GetBuildVersion(productName, dockerfileVersion, variables);
+            if (buildVersion is not null)
+            {
+                buildVersion = UpdateDependencies.ResolveProductVersion(buildVersion, options);
+            }
+
+            return buildVersion;
         }
     }
 }
