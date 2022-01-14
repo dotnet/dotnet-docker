550c288a1884090d5aef2b1d6a1e7b449766d511
Matt Thalman <mthalman@microsoft.com>
2022-01-14 10:07:34 -0600

    Add PowerShell 7.3.0-preview.1 to .NET 7 SDK Dockerfiles (#3359)

---
+++ tests/Microsoft.DotNet.Docker.Tests/HttpClientExtensions.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Microsoft.DotNet.Docker.Tests
+{
+    internal static class HttpClientExtensions
+    {
+        public static async Task DownloadFileAsync(this HttpClient client, Uri uri, string path)
+        {
+            using Stream stream = await client.GetStreamAsync(uri);
+            using FileStream fileStream = new(path, FileMode.OpenOrCreate);
+            await stream.CopyToAsync(fileStream);
+        }
+    }
+}
--- tests/Microsoft.DotNet.Docker.Tests/SdkImageTests.cs
+++ tests/Microsoft.DotNet.Docker.Tests/SdkImageTests.cs
@@ -8,6 +8,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 using SharpCompress.Common;
@@ -137,13 +138,7 @@ namespace Microsoft.DotNet.Docker.Tests
             }
 
             // Disable until PowerShell issue is fixed: https://github.com/PowerShell/PowerShell/issues/16532
-            if (imageData.Version.Major == 6 && imageData.OS == "alpine3.15")
-            {
-                return;
-            }
-
-            // Disable until PowerShell has support for 7.0
-            if (imageData.Version.Major == 7)
+            if (imageData.OS == "alpine3.15")
             {
                 return;
             }
@@ -274,33 +269,14 @@ namespace Microsoft.DotNet.Docker.Tests
 
         private async Task<IEnumerable<SdkContentFileInfo>> GetExpectedSdkContentsAsync(ProductImageData imageData)
         {
-            string sdkVersion = imageData.GetProductVersion(ImageType, DockerHelper);
-
-            string osType = DockerHelper.IsLinuxContainerModeEnabled ? "linux" : "win";
-            if (imageData.SdkOS.StartsWith(OS.AlpinePrefix))
-            {
-                osType += "-musl";
-            }
-
-            string fileType = DockerHelper.IsLinuxContainerModeEnabled ? "tar.gz" : "zip";
-
-            string architecture = imageData.Arch switch
-            {
-                Arch.Amd64 => "x64",
-                Arch.Arm => "arm",
-                Arch.Arm64 => "arm64",
-                _ => throw new InvalidOperationException($"Unexpected architecture type: '{imageData.Arch}'"),
-            };
-
-            string sdkUrl =
-                $"https://dotnetcli.azureedge.net/dotnet/Sdk/{sdkVersion}/dotnet-sdk-{sdkVersion}-{osType}-{architecture}.{fileType}";
+            string sdkUrl = GetSdkUrl(imageData);
 
             if (!s_sdkContentsCache.TryGetValue(sdkUrl, out IEnumerable<SdkContentFileInfo> files))
             {
                 string sdkFile = Path.GetTempFileName();
 
-                using WebClient webClient = new WebClient();
-                await webClient.DownloadFileTaskAsync(new Uri(sdkUrl), sdkFile);
+                using HttpClient httpClient = new();
+                await httpClient.DownloadFileAsync(new Uri(sdkUrl), sdkFile);
 
                 files = EnumerateArchiveContents(sdkFile)
                     .OrderBy(file => file.Path)
@@ -310,6 +286,36 @@ namespace Microsoft.DotNet.Docker.Tests
             }
 
             return files;
+        }
+
+        private string GetSdkUrl(ProductImageData imageData)
+        {
+            string sdkBuildVersion = Config.GetBuildVersion(ImageType, imageData.VersionString);
+            string sdkFileVersionLabel = sdkBuildVersion;
+
+            string osType = DockerHelper.IsLinuxContainerModeEnabled ? "linux" : "win";
+            if (imageData.SdkOS.StartsWith(OS.AlpinePrefix))
+            {
+                osType += "-musl";
+            }
+
+            string architecture = imageData.Arch switch
+            {
+                Arch.Amd64 => "x64",
+                Arch.Arm => "arm",
+                Arch.Arm64 => "arm64",
+                _ => throw new InvalidOperationException($"Unexpected architecture type: '{imageData.Arch}'"),
+            };
+
+            string fileType = DockerHelper.IsLinuxContainerModeEnabled ? "tar.gz" : "zip";
+
+            string baseUrl = "https://dotnetcli.azureedge.net/dotnet";
+            if (imageData.Version.Major >= 7)
+            {
+                baseUrl = Config.GetBaseUrl(imageData.VersionString);
+            }
+
+            return $"{baseUrl}/Sdk/{sdkBuildVersion}/dotnet-sdk-{sdkFileVersionLabel}-{osType}-{architecture}.{fileType}";
         }
 
         private void PowerShellScenario_Execute(ProductImageData imageData, string optionalArgs)
@@ -322,13 +328,7 @@ namespace Microsoft.DotNet.Docker.Tests
             }
 
             // Disable until PowerShell issue is fixed: https://github.com/PowerShell/PowerShell/issues/16532
-            if (imageData.Version.Major == 6 && imageData.OS == "alpine3.15")
-            {
-                return;
-            }
-
-            // Disable until PowerShell supports 7.0
-            if (imageData.Version.Major == 7)
+            if (imageData.OS == "alpine3.15")
             {
                 return;
             }
