63744addf3628d1ba54984497bec91793b62a2aa
Matt Thalman <mthalman@microsoft.com>
2022-02-01 09:14:36 -0600

    Remove Json as default console logging formatter in aspnet images for 7.0

--- tests/Microsoft.DotNet.Docker.Tests/AspnetImageTests.cs
+++ tests/Microsoft.DotNet.Docker.Tests/AspnetImageTests.cs
@@ -52,7 +52,7 @@ namespace Microsoft.DotNet.Docker.Tests
                 variables.Add(RuntimeImageTests.GetRuntimeVersionVariableInfo(imageData, DockerHelper));
             }
 
-            if (imageData.Version.Major >= 6)
+            if (imageData.Version.Major == 6)
             {
                 variables.Add(new EnvironmentVariableInfo("Logging__Console__FormatterName", "Json"));
             }
