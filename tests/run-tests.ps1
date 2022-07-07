SohamDesai27/asp.net-core-sample

Use multi-value OS versions for tests

tests/run-tests.ps1
@@ -8,7 +8,7 @@
 param(
     [string]$Version,
     [string]$Architecture,
-    [string]$OS,
+    [string[]]$OS,
     [string]$Registry,
     [string]$RepoPrefix,
     [switch]$DisableHttpVerification,
@@ -74,11 +74,6 @@ Try {
         $env:PULL_IMAGES = $null
     }
 
-    # By default, account for the OS having a -slim variant, except for mariner which has a -distroless variant that needs to be tested separately.
-    if (-not $OS.Contains("mariner")) {
-        $OS += "*"
-    }
-
     # Public builds group image build and test by runtime version.
     # Internal builds group image build by the same criteria but run tests in separate jobs that are grouped by image version.
     # The distinction is not apparent for images such as 'runtime', 'aspnet', and 'sdk' because
@@ -93,7 +88,7 @@ Try {
     }
 
     $env:IMAGE_ARCH = $Architecture
-    $env:IMAGE_OS = $OS
+    $env:IMAGE_OS_NAMES = $($OS -Join ",")
     $env:REGISTRY = $Registry
     $env:REPO_PREFIX = $RepoPrefix
     $env:IMAGE_INFO_PATH = $ImageInfoPath

dotnet/dotnet-docker

tests/run-tests.ps1
@@ -8,7 +8,7 @@
 param(
     [string]$Version,
     [string]$Architecture,
-    [string]$OS,
+    [string[]]$OSVersions,
     [string]$Registry,
     [string]$RepoPrefix,
     [switch]$DisableHttpVerification,
@@ -74,11 +74,6 @@ Try {
         $env:PULL_IMAGES = $null
     }
 
-    # By default, account for the OS having a -slim variant, except for mariner which has a -distroless variant that needs to be tested separately.
-    if (-not $OS.Contains("mariner")) {
-        $OS += "*"
-    }
-
     # Public builds group image build and test by runtime version.
     # Internal builds group image build by the same criteria but run tests in separate jobs that are grouped by image version.
     # The distinction is not apparent for images such as 'runtime', 'aspnet', and 'sdk' because
@@ -93,7 +88,7 @@ Try {
     }
 
     $env:IMAGE_ARCH = $Architecture
-    $env:IMAGE_OS = $OS
+    $env:IMAGE_OS_NAMES = $($OSVersions -Join ",")
     $env:REGISTRY = $Registry
     $env:REPO_PREFIX = $RepoPrefix
     $env:IMAGE_INFO_PATH = $ImageInfoPath