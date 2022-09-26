9803c4af675396a8a89faf4d76244717d2e295e6
Justin Anderson <jander-msft@users.noreply.github.com>
2022-09-26 06:14:46 -0700

    Add .NET Monitor 6.3 images; move Ubuntu Chiseled to 6.3 (#4089)

--- tests/Microsoft.DotNet.Docker.Tests/ImageVersion.cs
+++ tests/Microsoft.DotNet.Docker.Tests/ImageVersion.cs
@@ -11,6 +11,7 @@ namespace Microsoft.DotNet.Docker.Tests
         public static readonly Version V3_1 = new Version(3, 1);
         public static readonly Version V6_0 = new Version(6, 0);
         public static readonly Version V6_2 = new Version(6, 2);
+        public static readonly Version V6_3 = new Version(6, 3);
         public static readonly Version V7_0 = new Version(7, 0);
     }
 }
