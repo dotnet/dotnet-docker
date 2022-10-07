59c310319702bb1bd37fb48ec3a56d57aaa38539
Matt Thalman <mthalman@microsoft.com>
2022-10-07 14:04:41 -0500

    Set correct ownership of non-root user's home dir (#4115)
    (cherry picked from commit 0e80a1a305d86ad8506509b7423b4c6c3ec11185)

--- tests/Microsoft.DotNet.Docker.Tests/ImageScenarioVerifier.cs
+++ tests/Microsoft.DotNet.Docker.Tests/ImageScenarioVerifier.cs
@@ -89,7 +89,7 @@ namespace Microsoft.DotNet.Docker.Tests
             }
         }
 
-        private static void InjectCustomTestCode(string appDir)
+        private void InjectCustomTestCode(string appDir)
         {
             string programFilePath = Path.Combine(appDir, "Program.cs");
 
@@ -116,8 +116,13 @@ namespace Microsoft.DotNet.Docker.Tests
                     builder.Append(usingDir.ToFullString());
                 }
 
+                // Verify a web request succeeds
+                builder.AppendLine("System.Console.WriteLine(\"Verifying a web request succeeds\");");
                 builder.AppendLine("var response = await new System.Net.Http.HttpClient().GetAsync(\"https://www.microsoft.com\");");
                 builder.AppendLine("response.EnsureSuccessStatusCode();");
+
+                // Verify write access is allowed to the user directory
+                builder.AppendLine(GetUserDirectoryWriteAccessValidationCode());
 
                 foreach (SyntaxNode otherNode in otherNodes)
                 {
@@ -129,9 +134,13 @@ namespace Microsoft.DotNet.Docker.Tests
             else
             {
                 StatementSyntax testHttpsConnectivityStatement = SyntaxFactory.ParseStatement(
+                    // Verify a web request succeeds
+                    "System.Console.WriteLine(\"Verifying a web request succeeds\");" +
                     "var task = new System.Net.Http.HttpClient().GetAsync(\"https://www.microsoft.com\");" +
                     "task.Wait();" +
-                    "task.Result.EnsureSuccessStatusCode();");
+                    "task.Result.EnsureSuccessStatusCode();" +
+                    // Verify write access is allowed to the user directory
+                    GetUserDirectoryWriteAccessValidationCode());
 
                 MethodDeclarationSyntax newMainMethod = mainMethod.InsertNodesBefore(
                     mainMethod.Body.ChildNodes().First(),
@@ -142,6 +151,19 @@ namespace Microsoft.DotNet.Docker.Tests
             }
             
             File.WriteAllText(programFilePath, newContent);
+        }
+
+        private string GetUserDirectoryWriteAccessValidationCode()
+        {
+            if (_imageData.IsDistroless && _imageData.Version.Major == 6 && _imageData.OS.StartsWith(OS.Mariner))
+            {
+                return string.Empty;
+            }
+
+            string userDirEnvVarName = DockerHelper.IsLinuxContainerModeEnabled ? "HOME" : "USERPROFILE";
+            return
+                "System.Console.WriteLine(\"Verifying write access to user directory\");" +
+                $"System.IO.File.WriteAllText(System.Environment.GetEnvironmentVariable(\"{userDirEnvVarName}\") + \"/test.txt\", \"test\");";
         }
 
         private string BuildTestAppImage(string stageTarget, string contextDir, params string[] customBuildArgs)
