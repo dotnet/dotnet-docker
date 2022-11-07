cd375e1a2ed70ce9cce32c580d4e1396bf8013da
Matt Thalman <mthalman@microsoft.com>
2022-11-07 09:10:37 -0600

    Sync OS names

--- tests/Microsoft.DotNet.Docker.Tests/OS.cs
+++ tests/Microsoft.DotNet.Docker.Tests/OS.cs
@@ -30,6 +30,7 @@ namespace Microsoft.DotNet.Docker.Tests
         public const string Focal = "focal";
         public const string Jammy = "jammy";
         public const string JammyChiseled = "jammy-chiseled";
+        public const string UbuntuChiseled = "ubuntu-chiseled";
 
         // Windows
         public const string NanoServer = "nanoserver";
