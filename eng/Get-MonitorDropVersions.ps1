1a5ace620a1250d52eaf4476cf9f5e971be66ccb
Joe Schmitt <joschmit@microsoft.com>
2023-01-27 10:50:09 -0800

    Remove channel option

--- eng/Get-MonitorDropVersions.ps1
+++ eng/Get-MonitorDropVersions.ps1
@@ -6,24 +6,14 @@ Returns the various component versions of the latest .NET Monitor build.
 #>
 [cmdletbinding()]
 param(
-    # The release channel to use for determining the latest .NET Monitor build.
-    [Parameter(Mandatory=$true, ParameterSetName="WithChannel")]
-    [string]
-    $Channel,
-
     # A file containing the latest .NET Monitor build version.
-    [Parameter(Mandatory=$true, ParameterSetName="WithBuildVersionFile")]
+    [Parameter(Mandatory=$true)]
     [ValidateScript({if (-not (Test-Path -PathType Leaf -Path $(Resolve-Path $_).Path)) { throw "Could not find file $_"} $true})]
     [string]
     $BuildVersionFilePath
 )
 
-if ($Channel) {
-    $url = "https://aka.ms/dotnet/diagnostics/monitor$Channel/dotnet-monitor.nupkg.buildversion"
-    $monitorVersion = $(Invoke-RestMethod $url).Trim()
-} else {
-    $monitorVersion = $(Get-Content $BuildVersionFilePath).Trim()
-}
+$monitorVersion = $(Get-Content $BuildVersionFilePath).Trim()
 
 $versionSplit=$monitorVersion.Split('.', 3)
 $majorMinorVersion="$($versionSplit[0]).$($versionSplit[1])"
