578955b79214502308f6881168fd661bb51fc2c6
Rich Lander <rlander@microsoft.com>
2023-02-09 20:23:48 -0800

    Add basic HTTP health check (#4352)

--- samples/aspnetapp/aspnetapp/Program.cs
+++ samples/aspnetapp/aspnetapp/Program.cs
@@ -2,8 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
+app.MapHealthChecks("/healthz");
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -24,9 +26,32 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+
+CancellationTokenSource cancellation = new();
+app.Lifetime.ApplicationStopping.Register( () =>
+{
+    cancellation.Cancel();
+});
+
 app.MapGet("/Environment", () =>
 {
     return new EnvironmentInfo();
+});
+
+// This API demonstrates how to use task cancellation
+// to support graceful container shutdown via SIGTERM.
+// The method itself is an example and not useful.
+app.MapGet("/Delay/{value}", async (int value) =>
+{
+    try
+    {
+        await Task.Delay(value, cancellation.Token);
+    }
+    catch(TaskCanceledException)
+    {
+    }
+    
+    return new {Delay = value};
 });
 
 app.Run();
