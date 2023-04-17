7d5545253c4252e3406e9fbcb20a21a680221e78
Jaliya Udagedara <jaliya.udagedara@hotmail.com>
2023-04-18 05:20:16 +1200

    Using top level statements.

--- samples/complexapp/complexapp/Program.cs
+++ samples/complexapp/complexapp/Program.cs
@@ -2,16 +2,8 @@
 using libfoo;
 using static System.Console;
 
-namespace complexapp;
+string initialString = StringLibrary.GetString();
+string reversedString = StringUtils.ReverseString(initialString);
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        string initialString = StringLibrary.GetString();
-        string reversedString = StringUtils.ReverseString(initialString);
-
-        WriteLine($"string: {initialString}");
-        WriteLine($"reversed: {reversedString}");
-    }
-}
+WriteLine($"string: {initialString}");
+WriteLine($"reversed: {reversedString}");
