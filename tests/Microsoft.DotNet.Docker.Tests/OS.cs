61a3c299bfc2cb3bc28dea6622e60b76a9f1d4f5
Logan Bussell <loganbussell@microsoft.com>
2023-08-14 15:23:36 -0700

    Remove Alpine 3.17 Images (#4816)

--- tests/Microsoft.DotNet.Docker.Tests/OS.cs
+++ tests/Microsoft.DotNet.Docker.Tests/OS.cs
@@ -8,7 +8,6 @@ namespace Microsoft.DotNet.Docker.Tests
     {
         // Alpine
         public const string Alpine = "alpine";
-        public const string Alpine317 = $"{Alpine}3.17";
         public const string Alpine318 = $"{Alpine}3.18";
         public const string Alpine318Composite = $"{Alpine318}-composite";
 
