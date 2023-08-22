164d94c90406fd06d0397d0d4349a8404baa9823
Logan Bussell <loganbussell@microsoft.com>
2023-08-22 11:01:29 -0700

    Use HasFlag instead of bitwise logic

--- tests/Microsoft.DotNet.Docker.Tests/ProductImageData.cs
+++ tests/Microsoft.DotNet.Docker.Tests/ProductImageData.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -164,6 +164,6 @@ namespace Microsoft.DotNet.Docker.Tests
             return GetTagName(imageVersion.GetTagName(), os, variant);
         }
 
-        public bool ImageTypeIsSupported(DotNetImageType imageType) => (SupportedImageTypes & imageType) == imageType;
+        public bool ImageTypeIsSupported(DotNetImageType imageType) => SupportedImageTypes.HasFlag(imageType);
     }
 }
