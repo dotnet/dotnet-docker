f7cc6512ddfedcf6f7e5b5ca2ba7c5fade340676
Logan Bussell <loganbussell@microsoft.com>
2023-08-25 14:07:17 -0700

    Factor out Syft and package lists from tests

--- tests/Microsoft.DotNet.Docker.Tests/RuntimeDepsImageTests.cs
+++ tests/Microsoft.DotNet.Docker.Tests/RuntimeDepsImageTests.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using Microsoft.VisualBasic;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -73,6 +74,18 @@ namespace Microsoft.DotNet.Docker.Tests
             VerifyCommonDefaultUser(imageData);
         }
 
+        [LinuxImageTheory]
+        [MemberData(nameof(GetImageData))]
+        public void VerifyDistrolessOSReleaseInfo(ProductImageData imageData)
+        {
+            if (!imageData.IsDistroless)
+            {
+                return;
+            }
+
+            Assert.NotEmpty(GetOSReleaseInfo(imageData));
+        }
+
         /// <summary>
         /// Verifies that the packages installed in distroless images are scannable by security tools.
         /// </summary>
@@ -82,20 +95,76 @@ namespace Microsoft.DotNet.Docker.Tests
         {
             if (!imageData.IsDistroless)
             {
-                OutputHelper.WriteLine("This test is only relevant to distroless images.");
                 return;
             }
 
+            IEnumerable<string> basePackages = GetExpectedPackages(imageData);
+            IEnumerable<string> extraPackages = GetExpectedExtraPackages(imageData);
+
+            IEnumerable<string> expectedPackages = imageData.ImageVariant switch
+            {
+                DotNetImageVariant.Extra => basePackages.Concat(extraPackages).OrderBy(s => s),
+                _ => basePackages
+            };
+
+            IEnumerable<string> actualPackages = GetInstalledPackages(imageData);
+
+            OutputHelper.WriteLine($"Expected Packages: [ {string.Join(", ", expectedPackages)} ]");
+            OutputHelper.WriteLine($"Actual Packages: [ {string.Join(", ", actualPackages)} ]");
+
+            Assert.Equal(expectedPackages, actualPackages);
+        }
+
+        private IEnumerable<string> GetInstalledPackages(ProductImageData imageData)
+        {
+            JsonNode output = GetSyftOutput("package-info", imageData);
+            return ((JsonArray)output["artifacts"])
+                .Select(artifact => artifact["name"]?.ToString());
+        }
+
+        private string GetOSReleaseInfo(ProductImageData imageData)
+        {
+            JsonNode output = GetSyftOutput("os-release-info", imageData);
+            JsonObject distro = (JsonObject)output["distro"];
+            return (string)distro["version"];
+        }
+
+        private JsonNode GetSyftOutput(string name, ProductImageData imageData)
+        {
             const string SyftImage = "anchore/syft:v0.87.1";
             DockerHelper.Pull(SyftImage);
 
             string imageName = imageData.GetImage(ImageRepo, DockerHelper);
             string output = DockerHelper.Run(
-                SyftImage, "distroless-packages", $"packages docker:{imageName} -o json", useMountedDockerSocket: true);
+                SyftImage, name, $"packages docker:{imageName} -o json", useMountedDockerSocket: true);
 
-            string[] basePackages = imageData switch
+            return JsonNode.Parse(output)
+                    ?? throw new JsonException($"Unable to parse the output as JSON:{Environment.NewLine}{output}");
+        }
+
+        internal static IEnumerable<string> GetExpectedExtraPackages(ProductImageData imageData) => imageData switch
             {
-                {  OS: string os, Version: ImageVersion version }
+                { IsDistroless: true, OS: string os } when os.Contains(OS.Mariner) => new[]
+                    {
+                        "icu",
+                    },
+                { OS: OS.JammyChiseled } => new[]
+                    {
+                        "libicu70",
+                        "tzdata"
+                    },
+                { OS: string os } when os.Contains(OS.Alpine) => new[]
+                    {
+                        "icu-data-full",
+                        "icu-libs",
+                        "tzdata"
+                    },
+                _ => new string[0]
+            };
+
+        internal static IEnumerable<string> GetExpectedPackages(ProductImageData imageData) => imageData switch
+            {
+                { OS: string os, Version: ImageVersion version }
                         when (version.Major == 6 || version.Major == 7)
                              && os.Contains(OS.Mariner) => new[]
                     {
@@ -138,45 +207,6 @@ namespace Microsoft.DotNet.Docker.Tests
                     },
                 _ => throw new NotImplementedException()
             };
-
-            string[] extraPackages = imageData switch
-            {
-                { IsDistroless: true, OS: string os } when os.Contains(OS.Mariner) => new[]
-                    {
-                        "icu",
-                    },
-                { OS: OS.JammyChiseled } => new[]
-                    {
-                        "libicu70",
-                        "tzdata"
-                    },
-                _ => new string[0]
-            };
-
-            IEnumerable<string> expectedPackages = imageData switch
-            {
-                { ImageVariant: DotNetImageVariant.Extra } => basePackages.Concat(extraPackages).OrderBy(s => s),
-                _ => basePackages
-            };
-
-            JsonNode node = JsonNode.Parse(output);
-            if (node is null)
-            {
-                throw new JsonException($"Unable to parse the output as JSON:{Environment.NewLine}{output}");
-            }
-
-            IEnumerable<string> actualPackages = ((JsonArray)node["artifacts"])
-                .Select(artifact => artifact["name"]?.ToString());
-            
-            OutputHelper.WriteLine($"Expected Packages: [ {String.Join(", ", expectedPackages)} ]");
-            OutputHelper.WriteLine($"Actual Packages: [ {String.Join(", ", actualPackages)} ]");
-
-            Assert.Equal(expectedPackages, actualPackages);
-
-            // Verify the OS release info is available
-            JsonObject distro = (JsonObject)node["distro"];
-            Assert.NotEmpty((string)distro["version"]);
-        }
 
         internal static string[] GetExpectedRpmPackagesInstalled(ProductImageData imageData) =>
             new string[]
