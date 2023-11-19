5ff89c0325f849c3dd5555d8142ee03d787ac76d
Richard Lander <rlander@microsoft.com>
2023-11-19 11:44:48 -0800

    Update JSON schema

--- samples/releasesapi/ReleaseJson.cs
+++ samples/releasesapi/ReleaseJson.cs
@@ -2,32 +2,10 @@ using System.Text.Json.Serialization;
 
 namespace ReleaseJson;
 
-public record ReleaseIndex(
-    [property: JsonPropertyName("releases-index")] List<MajorRelease> ReleasesIndex
-    );
+public record ReleaseIndex(List<MajorRelease> ReleasesIndex);
 
-public record MajorRelease(
-    [property: JsonPropertyName("channel-version")] string ChannelVersion, 
-    [property: JsonPropertyName("latest-release")] string LatestRelease, 
-    [property: JsonPropertyName("latest-release-date")] string LatestReleaseDate, 
-    [property: JsonPropertyName("security")] bool Security, 
-    [property: JsonPropertyName("latest-runtime")] string LatestRuntime, 
-    [property: JsonPropertyName("latest-sdk")] string LatestSdk, 
-    [property: JsonPropertyName("release-type")] string ReleaseType, 
-    [property: JsonPropertyName("support-phase")] string SupportPhase, 
-    [property: JsonPropertyName("eol-date")] string EolDate, 
-    [property: JsonPropertyName("releases.json")] string ReleasesJson, 
-    [property: JsonPropertyName("releases")] List<ReleaseDetail> Releases
-    );
+public record MajorRelease(string ChannelVersion, string LatestRelease, string LatestReleaseDate, bool Security, string LatestRuntime, string LatestSdk, string ReleaseType, string SupportPhase, string EolDate, [property: JsonPropertyName("releases.json")] string ReleasesJson, List<Release> Releases);
 
-public record ReleaseDetail(
-    [property: JsonPropertyName("release-date")] string ReleaseDate, 
-    [property: JsonPropertyName("release-version")] string ReleaseVersion, 
-    [property: JsonPropertyName("security")] bool Security, 
-    [property: JsonPropertyName("cve-list")] List<Cve> Cves
-    );
+public record Release(string ReleaseDate, string ReleaseVersion, bool Security, List<Cve> CveList);
 
-public record Cve(
-    [property: JsonPropertyName("cve-id")] string CveId,
-    [property: JsonPropertyName("cve-url")] string CveUrl
-    );
+public record Cve(string CveId,string CveUrl);
