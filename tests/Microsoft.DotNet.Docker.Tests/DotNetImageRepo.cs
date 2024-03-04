ff4905aaa470e98eda2a27c5ea8f15bf9dcfb7ad
Logan Bussell <loganbussell@microsoft.com>
2024-03-04 12:09:32 -0800

    Add Aspire Dashboard image unit tests (#5226)
    Co-authored-by: Matt Thalman <mthalman@microsoft.com>

---
+++ tests/Microsoft.DotNet.Docker.Tests/DotNetImageRepo.cs
@@ -0,0 +1,18 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace Microsoft.DotNet.Docker.Tests;
+
+[Flags]
+public enum DotNetImageRepo
+{
+    SDK              = 1 << 0,
+    Runtime          = 1 << 1,
+    Runtime_Deps     = 1 << 2,
+    Aspnet           = 1 << 3,
+    Monitor          = 1 << 4,
+    Aspire_Dashboard = 1 << 5,
+}
