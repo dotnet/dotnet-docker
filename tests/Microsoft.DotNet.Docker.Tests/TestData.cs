d2797c00033ef07fb82e24945121a5f956202329
Logan Bussell <loganbussell@microsoft.com>
2024-05-21 11:10:35 -0700

    Update Aspire Dashboard to 8.0 (#5482)

--- tests/Microsoft.DotNet.Docker.Tests/TestData.cs
+++ tests/Microsoft.DotNet.Docker.Tests/TestData.cs
@@ -341,7 +341,7 @@ namespace Microsoft.DotNet.Docker.Tests
         private static readonly ProductImageData[] s_AspireDashboardTestData =
         {
             new() {
-                Version = V8_0_Preview,
+                Version = V8_0,
                 OS = OS.Mariner20Distroless,
                 OSTag = "",
                 OSDir = OS.MarinerDistroless,
@@ -349,7 +349,7 @@ namespace Microsoft.DotNet.Docker.Tests
                 SupportedImageRepos = DotNetImageRepo.Aspire_Dashboard,
             },
             new() {
-                Version = V8_0_Preview,
+                Version = V8_0,
                 OS = OS.Mariner20Distroless,
                 OSTag = "",
                 OSDir = OS.MarinerDistroless,
