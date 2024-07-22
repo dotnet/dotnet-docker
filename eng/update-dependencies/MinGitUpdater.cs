a5d857a0c4b9f3de8325e972cbd6dca852381f2c
Logan Bussell <loganbussell@microsoft.com>
2024-07-22 14:48:28 -0700

    Use Chisel binary instead of building from source (#5713)

--- eng/update-dependencies/MinGitUpdater.cs
+++ eng/update-dependencies/MinGitUpdater.cs
@@ -2,38 +2,22 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.DotNet.VersionTools.Dependencies;
-using Newtonsoft.Json.Linq;
+using Octokit;
 
 #nullable enable
 namespace Dotnet.Docker;
 
-internal abstract class MinGitUpdater : VariableUpdaterBase
+internal static class MinGitUpdater
 {
-    protected JObject LatestMinGitRelease { get; }
-
-    public MinGitUpdater(string repoRoot, JObject latestMinGitRelease, string variableName) : base(repoRoot, variableName)
+    public static async Task<IEnumerable<IDependencyUpdater>> GetMinGitUpdatersAsync(string repoRoot)
     {
-        LatestMinGitRelease = latestMinGitRelease;
+        Release minGitRelease = await GitHubHelper.GetLatestRelease("git-for-windows", "git");
+
+        return [
+            new MinGitUrlUpdater(repoRoot, minGitRelease),
+            new MinGitShaUpdater(repoRoot, minGitRelease),
+        ];
     }
-
-    protected sealed override string TryGetDesiredValue(IEnumerable<IDependencyInfo> dependencyInfos, out IEnumerable<IDependencyInfo> usedDependencyInfos)
-    {
-        IDependencyInfo? sdkDependencyInfo = dependencyInfos.FirstOrDefault(info => info.SimpleName == "sdk");
-
-        // Only update MinGit variables when we're updating the SDK Dockerfiles
-        if (sdkDependencyInfo is null)
-        {
-            usedDependencyInfos = Enumerable.Empty<IDependencyInfo>();
-            return ManifestHelper.ResolveVariableValue(VariableName, ManifestVariables.Value);
-        }
-
-        usedDependencyInfos = new[] { sdkDependencyInfo };
-        return GetValue();
-    }
-
-    protected abstract string GetValue();
-
 }
-#nullable disable
