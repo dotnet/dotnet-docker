8affb6778364f71e6f3393f4a51a6969f5da4160
Logan Bussell <loganbussell@microsoft.com>
2024-07-24 11:16:31 -0700

    Fix typo
    Co-authored-by: Omair Majid <omajid@redhat.com>

--- tests/run-tests.ps1
+++ tests/run-tests.ps1
@@ -113,7 +113,7 @@ Try {
     $env:IMAGE_ARCH = $Architecture
     $env:IMAGE_OS_NAMES = $($OSVersions -Join ",")
     $env:REGISTRY = $Registry
-    $env:CACHE_REGISTRY = $Registry
+    $env:CACHE_REGISTRY = $CacheRegistry
     $env:REPO_PREFIX = $RepoPrefix
     $env:IMAGE_INFO_PATH = $ImageInfoPath
     $env:SOURCE_REPO_ROOT = (Get-Item "$PSScriptRoot").Parent.FullName
