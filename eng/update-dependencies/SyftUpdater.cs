a4e8507d2943173bdd9a8a0abef78c1ee9ff0a8d
Nikola Milosavljevic <nikolam@microsoft.com>
2024-08-09 10:51:58 -0700

    Add syft updater
    Remove unused imports

---
+++ eng/update-dependencies/SyftUpdater.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Threading.Tasks;
+using Microsoft.DotNet.VersionTools.Dependencies;
+using Octokit;
+
+namespace Dotnet.Docker;
+
+#nullable enable
+internal static class SyftUpdater
+{
+    public static async Task<IDependencyUpdater> GetSyftUpdaterAsync(string repoRoot)
+    {
+        Release syftRelease = await GitHubHelper.GetLatestRelease("anchore", "syft");
+        return new GitHubReleaseVersionUpdater(repoRoot, "syft|tag", syftRelease, "dotnet");
+    }
+}
