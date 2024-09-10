86d3cbb28a65dba058569dc3672e8fac7dcc8d18
Logan Bussell <loganbussell@microsoft.com>
2024-09-10 10:07:23 -0700

    Clean up unnecessary test code

--- tests/Microsoft.DotNet.Docker.Tests/TestSolution.cs
+++ tests/Microsoft.DotNet.Docker.Tests/TestSolution.cs
@@ -56,13 +56,14 @@ public class TestSolution : IDisposable
             CreateProjectWithSdkImage("xunit", _testProjectDir, testProjectContainerName);
             File.Copy(Path.Combine(DockerHelper.TestArtifactsDir, "UnitTests.cs"), Path.Combine(_testProjectDir, "UnitTests.cs"));
 
-            string nuGetConfigFileName = "NuGet.config";
             if (Config.IsNightlyRepo)
             {
-                nuGetConfigFileName += ".nightly";
+                string nuGetConfigFileName = "NuGet.config.nightly";
+                File.Copy(
+                    Path.Combine(DockerHelper.TestArtifactsDir, nuGetConfigFileName),
+                    Path.Combine(solutionDir, "NuGet.config"));
             }
 
-            File.Copy(Path.Combine(DockerHelper.TestArtifactsDir, nuGetConfigFileName), Path.Combine(solutionDir, "NuGet.config"));
             File.Copy(Path.Combine(DockerHelper.TestArtifactsDir, ".dockerignore"), Path.Combine(solutionDir, ".dockerignore"));
         }
         catch (Exception)
