8b917f2cb56ade7ddbf6aa9bafa0ced27b63842e
Logan Bussell <loganbussell@microsoft.com>
2024-10-29 12:49:13 -0700

    Add globalization scenario test (#6012)

--- tests/Microsoft.DotNet.Docker.Tests/RuntimeImageTests.cs
+++ tests/Microsoft.DotNet.Docker.Tests/RuntimeImageTests.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.DotNet.Docker.Tests.TestScenarios;
 using Xunit;
 using Xunit.Abstractions;
 
