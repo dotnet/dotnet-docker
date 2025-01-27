aebb50f2feeb110619b82f96e1c62538a933064a
Logan Bussell <loganbussell@microsoft.com>
2025-01-27 12:28:56 -0800

    Add comment for PowerShell support on Alpine Arm

--- tests/Microsoft.DotNet.Docker.Tests/ProductImageData.cs
+++ tests/Microsoft.DotNet.Docker.Tests/ProductImageData.cs
@@ -20,6 +20,9 @@ namespace Microsoft.DotNet.Docker.Tests
 
         public bool GlobalizationInvariantMode => !SupportsGlobalization;
 
+        // PowerShell does not support Arm-based Alpine, skip testing
+        // https://github.com/PowerShell/PowerShell/issues/14667
+        // https://github.com/PowerShell/PowerShell/issues/12937
         public bool SupportsPowerShell => !(OS.Contains("alpine") && IsArm);
 
         public string SdkOS
