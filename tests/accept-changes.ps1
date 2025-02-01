db0af85bfd66c9ff78785d74f6d12c2882d4da81
Logan Bussell <loganbussell@microsoft.com>
2025-01-31 17:07:44 -0800

    Add helper script and documentation

--- tests/accept-changes.ps1
+++ tests/accept-changes.ps1
@@ -4,8 +4,17 @@
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 #
 
-Get-ChildItem `
+param (
+    [switch]$Discard
+)
+
+$files = Get-ChildItem `
     -Recurse `
     -Path "$PSScriptRoot/Microsoft.DotNet.Docker.Tests/Baselines" `
-    -Filter "*.received.txt" `
-        | Rename-Item -NewName { $_.Name -replace '\.received\.txt$', '.approved.txt' }
+    -Filter "*.received.txt"
+
+if ($Discard) {
+    $files | Remove-Item
+} else {
+    $files | Rename-Item -NewName { $_.Name -replace '\.received\.txt$', '.approved.txt' }
+}
