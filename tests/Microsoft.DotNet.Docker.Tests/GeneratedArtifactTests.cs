f2b8a6b45f5507fd09fb1b39b6aa472f4d489e3f
Logan Bussell <loganbussell@microsoft.com>
2025-02-03 10:19:50 -0800

    Rearrange helper methods for generated artifact tests

--- tests/Microsoft.DotNet.Docker.Tests/GeneratedArtifactTests.cs
+++ tests/Microsoft.DotNet.Docker.Tests/GeneratedArtifactTests.cs
@@ -11,6 +11,8 @@ using Shouldly.Configuration;
 using Xunit;
 using Xunit.Abstractions;
 using Microsoft.DotNet.Docker.Tests.Extensions;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Microsoft.DotNet.Docker.Tests
 {
@@ -55,6 +57,7 @@ namespace Microsoft.DotNet.Docker.Tests
             string dockerfilesOutputDirectory = Path.Combine(outputDirectory.Path, "src");
             string errorMessage = $"Failed to generate Dockerfiles using `{s_generateDockerfilesScript}`.";
 
+            // Override base URLs to make templates generate internal versions of Dockerfiles
             const string InternalBaseUrl = "https://artifacts.visualstudio.com";
             string customImageBuilderArgs =
                 $" --var 'base-url|public|maintenance|main={InternalBaseUrl}'" +
@@ -71,11 +74,43 @@ namespace Microsoft.DotNet.Docker.Tests
             string[] dockerfilePaths = DockerfileHelper.GetAllDockerfilesInDirectory(dockerfilesOutputDirectory);
 
             // Set up configuration for approval tests
+            List<(Regex pattern, string replacement)> replacePatterns =
+            [
+                (DockerfileHelper.Sha512Regex, "{sha512_placeholder}"),
+                (DockerfileHelper.Sha386Regex, "{sha386_placeholder}"),
+                (DockerfileHelper.Sha256Regex, "{sha256_placeholder}"),
+                (DockerfileHelper.SemanticVersionRegex, "0.0.0"),
+                (DockerfileHelper.MinGitVersionRegex, "v0.0.0.windows.0"),
+                (DockerfileHelper.AlpineVersionRegex, "alpine3.XX"),
+            ];
+
+            string DockerfileScrubber(string content)
+            {
+                foreach ((Regex pattern, string replacement) in replacePatterns)
+                {
+                    content = pattern.Replace(content, replacement);
+                }
+
+                return content;
+            }
+
             string GetDiscriminator(string dockerfilePath) =>
                 dockerfilePath
                     .Remove(0, dockerfilesOutputDirectory.Length + 1)
                     .Replace('/', '-')
                     .Replace('\\', '-');
+
+            string MatchTestFilenameGenerator(
+                TestMethodInfo testMethodInfo,
+                string discriminator,
+                string fileType,
+                string fileExtension) =>
+                    Path.Combine(
+                        testMethodInfo.DeclaringTypeName,
+                        testMethodInfo.MethodName,
+                        // The discriminator identifies the Dockerfile.
+                        // Remove the first character to remove the leading '.'
+                        $"{discriminator[1..]}.{fileType}.{fileExtension}");
 
             Action<ShouldMatchConfigurationBuilder> Configure(string dockerfilePath) =>
                 (ShouldMatchConfigurationBuilder config) =>
@@ -91,25 +126,13 @@ namespace Microsoft.DotNet.Docker.Tests
                         // which needs to be unique per Dockerfile
                         .WithDiscriminator(GetDiscriminator(dockerfilePath))
                         .WithFilenameGenerator(MatchTestFilenameGenerator)
-                        .WithScrubber(DockerfileHelper.DockerfileScrubber);
+                        .WithScrubber(DockerfileScrubber);
                 };
 
             // Validate the generated Dockerfiles
             dockerfilePaths.ShouldAllSatisfy(
                 path => File.ReadAllText(path).ShouldMatchApproved(configureOptions: Configure(path)));
         }
-
-        private static string MatchTestFilenameGenerator(
-            TestMethodInfo testMethodInfo,
-            string discriminator,
-            string fileType,
-            string fileExtension) =>
-                Path.Combine(
-                    testMethodInfo.DeclaringTypeName,
-                    testMethodInfo.MethodName,
-                    // The discriminator identifies the Dockerfile.
-                    // Remove the first character to remove the leading '.'
-                    $"{discriminator[1..]}.{fileType}.{fileExtension}");
 
         private void ValidateGeneratedArtifacts(string generateScriptPath, string errorMessage)
         {
