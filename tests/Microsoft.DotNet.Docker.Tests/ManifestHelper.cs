33c1bfd746c4e553c5714b8fb9155c96ba195ee9
Logan Bussell <loganbussell@microsoft.com>
2025-02-05 11:11:04 -0800

    Convert to file-scoped namespace

--- tests/Microsoft.DotNet.Docker.Tests/ManifestHelper.cs
+++ tests/Microsoft.DotNet.Docker.Tests/ManifestHelper.cs
@@ -10,68 +10,67 @@ using System.Text.RegularExpressions;
 
 #nullable enable
 
-namespace Microsoft.DotNet.Docker.Tests
+namespace Microsoft.DotNet.Docker.Tests;
+
+public static class ManifestHelper
 {
-    public static class ManifestHelper
+    public record DockerfileInfo(string Repo, string MajorMinor, string Os, string Architecture);
+
+    public static Manifest GetManifest() =>
+        JsonConvert.DeserializeObject<Manifest>(Config.Manifest.Value.ToString()) ??
+        throw new Exception("Failed to deserialize manifest");
+
+    public static string GetResolvedProductVersion(Image image) =>
+        GetVariableValue(image.ProductVersion);
+
+    public static IEnumerable<string> GetResolvedProductVersions(Repo repo) =>
+        repo.Images.Select(GetResolvedProductVersion).Distinct();
+
+    public static List<string> GetResolvedSharedTags(Image image) =>
+        image.SharedTags.Keys.Select(GetVariableValue).ToList();
+
+    public static List<string> GetResolvedTags(Platform platform) =>
+        platform.Tags.Keys.Select(GetVariableValue).ToList();
+
+    private static readonly string DockerfileRegex = @"src/(?<repo>.+)/(?<major_minor>\d+\.\d+)/(?<os>.+)/(?<architecture>.+)";
+
+    public static Dictionary<DockerfileInfo, List<string>> GetDockerfileTags(Repo repo)
     {
-        public record DockerfileInfo(string Repo, string MajorMinor, string Os, string Architecture);
-
-        public static Manifest GetManifest() =>
-            JsonConvert.DeserializeObject<Manifest>(Config.Manifest.Value.ToString()) ??
-            throw new Exception("Failed to deserialize manifest");
-
-        public static string GetResolvedProductVersion(Image image) =>
-            GetVariableValue(image.ProductVersion);
-
-        public static IEnumerable<string> GetResolvedProductVersions(Repo repo) =>
-            repo.Images.Select(GetResolvedProductVersion).Distinct();
-
-        public static List<string> GetResolvedSharedTags(Image image) =>
-            image.SharedTags.Keys.Select(GetVariableValue).ToList();
-
-        public static List<string> GetResolvedTags(Platform platform) =>
-            platform.Tags.Keys.Select(GetVariableValue).ToList();
-
-        private static readonly string DockerfileRegex = @"src/(?<repo>.+)/(?<major_minor>\d+\.\d+)/(?<os>.+)/(?<architecture>.+)";
-
-        public static Dictionary<DockerfileInfo, List<string>> GetDockerfileTags(Repo repo)
+        Dictionary<DockerfileInfo, List<string>> dockerfileTags = new Dictionary<DockerfileInfo, List<string>>();
+        foreach (Image image in repo.Images)
         {
-            Dictionary<DockerfileInfo, List<string>> dockerfileTags = new Dictionary<DockerfileInfo, List<string>>();
-            foreach (Image image in repo.Images)
+            foreach (Platform platform in image.Platforms)
             {
-                foreach (Platform platform in image.Platforms)
+                DockerfileInfo dockerfileInfo = GetDockerfileInfo(platform.Dockerfile);
+                if (!dockerfileTags.ContainsKey(dockerfileInfo))
                 {
-                    DockerfileInfo dockerfileInfo = GetDockerfileInfo(platform.Dockerfile);
-                    if (!dockerfileTags.ContainsKey(dockerfileInfo))
-                    {
-                        dockerfileTags[dockerfileInfo] = new List<string>();
-                    }
-                    dockerfileTags[dockerfileInfo].AddRange(GetResolvedTags(platform));
-                    dockerfileTags[dockerfileInfo].AddRange(GetResolvedSharedTags(image));
+                    dockerfileTags[dockerfileInfo] = new List<string>();
                 }
+                dockerfileTags[dockerfileInfo].AddRange(GetResolvedTags(platform));
+                dockerfileTags[dockerfileInfo].AddRange(GetResolvedSharedTags(image));
             }
-            return dockerfileTags;
         }
+        return dockerfileTags;
+    }
 
-        public static DockerfileInfo GetDockerfileInfo(string dockerfile)
+    public static DockerfileInfo GetDockerfileInfo(string dockerfile)
+    {
+        var match = Regex.Match(dockerfile, DockerfileRegex);
+        if (!match.Success)
         {
-            var match = Regex.Match(dockerfile, DockerfileRegex);
-            if (!match.Success)
-            {
-                throw new Exception($"Failed to parse dockerfile: {dockerfile}");
-            }
-            return new DockerfileInfo(match.Groups["repo"].Value, match.Groups["major_minor"].Value, match.Groups["os"].Value, match.Groups["architecture"].Value);
+            throw new Exception($"Failed to parse dockerfile: {dockerfile}");
         }
+        return new DockerfileInfo(match.Groups["repo"].Value, match.Groups["major_minor"].Value, match.Groups["os"].Value, match.Groups["architecture"].Value);
+    }
 
-        private static string GetVariableValue(string input)
+    private static string GetVariableValue(string input)
+    {
+        string variablePattern = @"\$\((?<variable>.+)\)";
+        foreach (Match match in Regex.Matches(input, variablePattern))
         {
-            string variablePattern = @"\$\((?<variable>.+)\)";
-            foreach (Match match in Regex.Matches(input, variablePattern))
-            {
-                string variableName = Config.GetVariableValue(match.Groups["variable"].Value);
-                input = input.Replace(match.Value, variableName);
-            }
-            return input;
+            string variableName = Config.GetVariableValue(match.Groups["variable"].Value);
+            input = input.Replace(match.Value, variableName);
         }
+        return input;
     }
 }
