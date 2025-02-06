acb9b503d71bb869ed5b035fbce82a8ee50be40d
Logan Bussell <loganbussell@microsoft.com>
2025-02-06 08:11:34 -0800

    Remove unused imports

--- tests/Microsoft.DotNet.Docker.Tests/TestDataTests.cs
+++ tests/Microsoft.DotNet.Docker.Tests/TestDataTests.cs
@@ -3,10 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using FluentAssertions;
 using Shouldly;
 using Xunit;
 using Xunit.Abstractions;
