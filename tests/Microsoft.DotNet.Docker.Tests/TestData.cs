35baa88b7add426f31b70c114099220ebd5a5e35
Logan Bussell <loganbussell@microsoft.com>
2025-02-26 10:21:35 -0800

    Update Aspire Dashboard image to Azure Linux 3.0

--- tests/Microsoft.DotNet.Docker.Tests/TestData.cs
+++ tests/Microsoft.DotNet.Docker.Tests/TestData.cs
@@ -413,18 +413,18 @@ namespace Microsoft.DotNet.Docker.Tests
             new() {
                 Version = V9_1_Preview,
                 VersionFamily = V9_0,
-                OS = OS.Mariner20Distroless,
+                OS = OS.AzureLinux30Distroless,
                 OSTag = "",
-                OSDir = OS.MarinerDistroless,
+                OSDir = OS.AzureLinuxDistroless,
                 Arch = Arch.Amd64,
                 SupportedImageRepos = DotNetImageRepo.Aspire_Dashboard,
             },
             new() {
                 Version = V9_1_Preview,
                 VersionFamily = V9_0,
-                OS = OS.Mariner20Distroless,
+                OS = OS.AzureLinux30Distroless,
                 OSTag = "",
-                OSDir = OS.MarinerDistroless,
+                OSDir = OS.AzureLinuxDistroless,
                 Arch = Arch.Arm64,
                 SupportedImageRepos = DotNetImageRepo.Aspire_Dashboard
             },
