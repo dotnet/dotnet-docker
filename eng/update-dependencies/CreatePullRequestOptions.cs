b0ad6d8a287aec8a1698b337467499860aecfcd6
Logan Bussell <loganbussell@microsoft.com>
2025-04-18 09:15:11 -0700

    Factor out PR options from specific command

--- eng/update-dependencies/CreatePullRequestOptions.cs
+++ eng/update-dependencies/CreatePullRequestOptions.cs
@@ -10,12 +10,13 @@ public abstract class CreatePullRequestOptions
 {
     private string? _targetBranch = null;
 
-    public string? Owner { get; init; } = null;
-    public string? Project { get; init; } = null;
-    public string? User { get; init; } = null;
-    public string? Email { get; init; } = null;
-    public string? Password { get; init; } = null;
-
+    public string User { get; init; } = "";
+    public string Email { get; init; } = "";
+    public string Password { get; init; } = "";
+    public string AzdoOrganization { get; init; } = "";
+    public string AzdoProject { get; init; } = "";
+    public string AzdoRepo { get; init; } = "";
+    public string VersionSourceName { get; init; } = "";
     public string SourceBranch { get; init; } = "nightly";
     public string TargetBranch
     {
@@ -25,13 +26,15 @@ public abstract class CreatePullRequestOptions
 
     public static List<Option> Options =>
     [
-        new Option<string>("--owner") { Description = "Owner of the fork where the branch should be pushed" },
-        new Option<string>("--project") { Description = "Name of the repo" },
-        new Option<string>("--user") { Description = "GitHub login used to create PR" },
-        new Option<string>("--email") { Description = "GitHub email used to create PR" },
-        new Option<string>("--password") { Description = "GitHub password used to create PR" },
-        new Option<string>("--source-branch") { Description = "Source branch from which the changes are derived" },
-        new Option<string>("--target-branch") { Description = "Target branch for the generated PR (defaults to --source-branch)" },
+        new Option<string>("--user") { Description = "GitHub or AzDO user used to make PR (if not specified, a PR will not be created)" },
+        new Option<string>("--email") { Description = "GitHub or AzDO email used to make PR (if not specified, a PR will not be created)" },
+        new Option<string>("--password") { Description = "GitHub or AzDO password used to make PR (if not specified, a PR will not be created)" },
+        new Option<string>("--azdo-organization", "--org") { Description = "Name of the AzDO organization" },
+        new Option<string>("--azdo-project", "--project") { Description = "Name of the AzDO project" },
+        new Option<string>("--azdo-repo", "--repo") { Description = "Name of the AzDO repo" },
+        new Option<string>("--version-source-name") { Description = "The name of the source from which the version information was acquired." },
+        new Option<string>("--source-branch") { Description = "Branch where the Dockerfiles are hosted" },
+        new Option<string>("--target-branch") { Description = "Target branch of the generated PR (defaults to value of source-branch)" },
     ];
 
     public static List<Argument> Arguments => [];
