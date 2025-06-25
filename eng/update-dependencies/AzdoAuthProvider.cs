209b659af87fddff8284e8690bc8c338fa6d163b
Logan Bussell <loganbussell@microsoft.com>
2025-06-25 10:14:07 -0700

    Add AzdoAuthProvider

---
+++ eng/update-dependencies/AzdoAuthProvider.cs
@@ -0,0 +1,37 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using Azure.Identity;
+
+namespace Dotnet.Docker;
+
+public class AzdoAuthProvider
+{
+    /// <summary>
+    /// This scope provides access to Azure DevOps Services REST API.
+    /// </summary>
+    /// <remarks>
+    /// See https://learn.microsoft.com/en-us/rest/api/azure/devops/tokens/?view=azure-devops-rest-7.1&tabs=powershell#personal-access-tokens-pats
+    /// </remarks
+    private const string Scope = "499b84ac-1321-427f-aa17-267ca6975798/.default";
+
+    /// <summary>
+    /// Gets an Azure DevOps access token. Defaults to SYSTEM_ACCESSTOKEN
+    /// environment variable, then falls back to using the Azure Developer CLI
+    /// credential to get a PAT.
+    /// </summary>
+    /// <returns></returns>
+    public string GetAccessToken()
+    {
+        string? accessToken = Environment.GetEnvironmentVariable("SYSTEM_ACCESSTOKEN");
+        if (!string.IsNullOrWhiteSpace(accessToken))
+        {
+            return accessToken;
+        }
+
+        var credential = new AzureDeveloperCliCredential();
+        accessToken = credential.GetToken(new Azure.Core.TokenRequestContext(scopes: [Scope])).Token;
+        return accessToken;
+    }
+}
