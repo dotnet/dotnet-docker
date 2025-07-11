a68830dcf6ec4cf5c613a3fc25f5265a2b81c5e5
Logan Bussell <loganbussell@microsoft.com>
2025-07-11 15:17:54 -0700

    Add extra properties to DotNetVersion

--- eng/update-dependencies/DotNetVersion.cs
+++ eng/update-dependencies/DotNetVersion.cs
@@ -1,15 +1,20 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using System.Text.RegularExpressions;
+
 namespace Dotnet.Docker;
 
 internal partial class DotNetVersion
 {
     private readonly string _versionString;
+    private readonly Lazy<Match> _match;
 
     public DotNetVersion(string versionString)
     {
         _versionString = versionString;
+        _match = new(() => SemanticVersionRegex.Match(_versionString));
     }
 
     // Allow a string to be implicitly converted to DotNetVersion
@@ -17,4 +22,37 @@ internal partial class DotNetVersion
 
     // Allow implicit conversion to string
     public override string ToString() => _versionString;
+
+    /// <summary>
+    /// .NET major version (e.g. "8", "9", or "10")
+    /// </summary>
+    public string Major => _match.Value.Groups["major"].Value;
+
+    /// <summary>
+    /// .NET minor version (usually "0")
+    /// </summary>
+    public string Minor => _match.Value.Groups["minor"].Value;
+
+    /// <summary>
+    /// .NET patch version. For runtime versions, this is number typically
+    /// starts at 0, and for SDK versions, it typically starts at 100, 200,
+    /// 300, or 400.
+    /// </summary>
+    public string Patch => _match.Value.Groups["patch"].Value;
+
+    /// <summary>
+    /// Refers to the "prerelease" portion of the version string in semantic
+    /// versioning terms. For stable release .NET versions, this is typically
+    /// empty. For preview .NET versions, it is usually a suffix like
+    /// "preview.6.12345.678-shipping". For internal builds of stable verisons,
+    /// it can be a suffix like "servicing.12345.6".
+    /// </summary>
+    /// <remarks>
+    /// Does not include the leading hyphen.
+    /// </remarks>
+    public string Prerelease => _match.Value.Groups["prerelease"].Value;
+
+    // Semantic version 2.0.0 regex from https://semver.org/
+    [GeneratedRegex(@"^(?<major>0|[1-9]\d*)\.(?<minor>0|[1-9]\d*)\.(?<patch>0|[1-9]\d*)(?:-(?<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$")]
+    private static partial Regex SemanticVersionRegex { get; }
 }
