4209ba6761677f854f8fcba52f534f5b519a0399
Logan Bussell <loganbussell@microsoft.com>
2025-07-17 14:25:07 -0700

    Use staged version of checksums for internal Dockerfile baselines

--- tests/Microsoft.DotNet.Docker.Tests/GeneratedArtifactTests.cs
+++ tests/Microsoft.DotNet.Docker.Tests/GeneratedArtifactTests.cs
@@ -71,7 +71,8 @@ public class GeneratedArtifactTests
             $" --var 'base-url|public|maintenance|main={InternalBaseUrl}'" +
             $" --var 'base-url|public|maintenance|nightly={InternalBaseUrl}'" +
             $" --var 'base-url|public|preview|main={InternalBaseUrl}'" +
-            $" --var 'base-url|public|preview|nightly={InternalBaseUrl}'";
+            $" --var 'base-url|public|preview|nightly={InternalBaseUrl}'" +
+            $" --var 'base-url|public-checksums|preview|nightly={InternalBaseUrl}'";
 
         // Generate internal Dockerfiles
         ExecuteScript(
