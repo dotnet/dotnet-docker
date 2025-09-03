78e55f76c83d47551909a1dfe8a3fb48f8be1fbb
Wiktor Kopec <wiktork@microsoft.com>
2025-09-03 10:05:47 -0700

    Pr feedback

--- tests/Microsoft.DotNet.Docker.Tests/TestData.cs
+++ tests/Microsoft.DotNet.Docker.Tests/TestData.cs
@@ -360,8 +360,8 @@ namespace Microsoft.DotNet.Docker.Tests
             new ProductImageData { Version = V9_0, VersionFamily = V9_0, OS = OS.AzureLinux30Distroless, OSDir = OS.AzureLinuxDistroless, OSTag = "", Arch = Arch.Arm64,  SupportedImageRepos = DotNetImageRepo.Monitor | DotNetImageRepo.Monitor_Base },
             new ProductImageData { Version = V9_1_Preview, VersionFamily = V9_0, OS = OS.AzureLinux30Distroless, OSDir = OS.AzureLinuxDistroless, OSTag = "", Arch = Arch.Amd64,  SupportedImageRepos = DotNetImageRepo.Monitor | DotNetImageRepo.Monitor_Base },
             new ProductImageData { Version = V9_1_Preview, VersionFamily = V9_0, OS = OS.AzureLinux30Distroless, OSDir = OS.AzureLinuxDistroless, OSTag = "", Arch = Arch.Arm64,  SupportedImageRepos = DotNetImageRepo.Monitor | DotNetImageRepo.Monitor_Base },
-            new ProductImageData { Version = V10_0, VersionFamily = V10_0, OS = OS.AzureLinux30Distroless, OSDir = OS.AzureLinuxDistroless, OSTag = "", Arch = Arch.Amd64,  SupportedImageRepos = DotNetImageRepo.Monitor | DotNetImageRepo.Monitor_Base },
-            new ProductImageData { Version = V10_0, VersionFamily = V10_0, OS = OS.AzureLinux30Distroless, OSDir = OS.AzureLinuxDistroless, OSTag = "", Arch = Arch.Arm64,  SupportedImageRepos = DotNetImageRepo.Monitor | DotNetImageRepo.Monitor_Base },
+            new ProductImageData { Version = V10_0_Preview, VersionFamily = V10_0, OS = OS.AzureLinux30Distroless, OSDir = OS.AzureLinuxDistroless, OSTag = "", Arch = Arch.Amd64,  SupportedImageRepos = DotNetImageRepo.Monitor | DotNetImageRepo.Monitor_Base },
+            new ProductImageData { Version = V10_0_Preview, VersionFamily = V10_0, OS = OS.AzureLinux30Distroless, OSDir = OS.AzureLinuxDistroless, OSTag = "", Arch = Arch.Arm64,  SupportedImageRepos = DotNetImageRepo.Monitor | DotNetImageRepo.Monitor_Base },
         ];
 
         private static readonly ProductImageData[] s_windowsMonitorTestData =
