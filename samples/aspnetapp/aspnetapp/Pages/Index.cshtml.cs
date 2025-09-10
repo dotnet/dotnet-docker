5383e83cca64858e050b844a24f226358ab77a85
Logan Bussell <loganbussell@microsoft.com>
2025-09-10 11:09:36 -0700

    Update aspnetapp sample code from latest razor template

--- samples/aspnetapp/aspnetapp/Pages/Index.cshtml.cs
+++ samples/aspnetapp/aspnetapp/Pages/Index.cshtml.cs
@@ -5,13 +5,6 @@ namespace aspnetapp.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
-
     public void OnGet()
     {
 
