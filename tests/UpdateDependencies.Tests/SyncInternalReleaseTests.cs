2fa13fc234b1bead987e1179a49fbf6834188ae4
Logan Bussell <loganbussell@microsoft.com>
2025-09-24 14:14:38 -0700

    Clean up tests

--- tests/UpdateDependencies.Tests/SyncInternalReleaseTests.cs
+++ tests/UpdateDependencies.Tests/SyncInternalReleaseTests.cs
@@ -11,14 +11,17 @@ public sealed class SyncInternalReleaseTests
 {
     private const string ReleaseBranch = "release/1";
     private const string InternalReleaseBranch = $"internal/{ReleaseBranch}";
-    private const string ArbitraryInternalBranch = "internal/foo";
-
     private const string AzdoOrg = "test-org";
     private const string AzdoProject = "test-project";
     private const string AzdoRepo = "test-repo";
     private const string RemoteAzdoUrl = $"https://dev.azure.com/{AzdoOrg}/{AzdoProject}/_git/{AzdoRepo}";
 
-    private static readonly SyncInternalReleaseOptions DefaultOptions = new()
+    /// <summary>
+    /// Default set of command options with some correct values. These can be used in most tests.
+    /// The primary thing that changes the behavior of the command is the state of the remote git
+    /// repository, which is mocked in the tests.
+    /// </summary>
+    private static readonly SyncInternalReleaseOptions s_defaultOptions = new()
     {
         RemoteUrl = RemoteAzdoUrl,
         SourceBranch = ReleaseBranch,
@@ -26,12 +29,14 @@ public sealed class SyncInternalReleaseTests
     };
 
     /// <summary>
-    /// If the currently checked out branch is an internal branch (i.e. "internal/foo"), the command should fail.
+    /// If the source branch is an internal branch (i.e. "internal/foo"), the command should fail.
+    /// Internal branches should always be the target branch of a sync operation, never the source
+    /// branch.
     /// </summary>
     [Fact]
     public async Task InternalSourceBranchFails()
     {
-        var options = DefaultOptions with { SourceBranch = ArbitraryInternalBranch };
+        var options = s_defaultOptions with { SourceBranch = "internal/foo" };
 
         var command = new SyncInternalReleaseCommand(
             Mock.Of<IGitRepoHelperFactory>(),
@@ -41,14 +46,13 @@ public sealed class SyncInternalReleaseTests
     }
 
     /// <summary>
-    /// If we start on a release/* branch and the corresponding internal/release/* branch does not exist,
-    /// then it should be created with the same commit as the release branch.
+    /// If we start on a release/* branch and the corresponding internal/release/* branch does not
+    /// exist, then it should be created with the same commit as the release branch.
     /// </summary>
-    /// <returns></returns>
     [Fact]
     public async Task CreateInternalBranch()
     {
-        var options = DefaultOptions;
+        var options = s_defaultOptions;
 
         var repoMock = new Mock<IGitRepoHelper>();
         var repoFactoryMock = new Mock<IGitRepoHelperFactory>();
@@ -72,13 +76,13 @@ public sealed class SyncInternalReleaseTests
     }
 
     /// <summary>
-    /// If the internal release branch already matches the release branch, then nothing should happen.
-    /// The command should not fail.
+    /// If the internal release branch already matches the release branch, then nothing should
+    /// happen. The command should not fail.
     /// </summary>
     [Fact]
     public async Task AlreadyUpToDate()
     {
-        var options = DefaultOptions;
+        var options = s_defaultOptions;
 
         // Strict mock behavior ensures that no extra calls are made that are
         // not explicitly set up in this test.
