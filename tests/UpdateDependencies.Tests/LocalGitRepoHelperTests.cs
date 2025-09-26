2c6692a853baf08debdec7f033bd892622898448
Logan Bussell <loganbussell@microsoft.com>
2025-09-26 10:03:28 -0700

    Also set committer identity

--- tests/UpdateDependencies.Tests/LocalGitRepoHelperTests.cs
+++ tests/UpdateDependencies.Tests/LocalGitRepoHelperTests.cs
@@ -16,18 +16,25 @@ public sealed class LocalGitRepoHelperTests
         const string DescendantBranch = "descendant";
 
         // example.com is safe for use in testing - it is reserved per RFC2606
-        var testIdentity = (Name: "Automation", Email: "test@example.com");
+        var authorIdentity = (Name: "Test Author", Email: "test@example.com");
+        var committerIdentity = (Name: "Test Committer", Email: "test@example.com");
 
         var repo = await CreateLocalRepoHelperAsync(
             async localRepo =>
             {
                 await localRepo.ExecuteGitCommand("init");
 
+                // Commit author and committer identity are separate concepts.
+                // `author:` argument below sets the author identity for the commit, but we also
+                // need to set the committer identity beforehand.
+                await localRepo.ExecuteGitCommand("config", "user.name", committerIdentity.Name);
+                await localRepo.ExecuteGitCommand("config", "user.email", committerIdentity.Email);
+
                 // Create initial commit on main branch
                 var initialFilePath = Path.Join(localRepo.Path, "initialFile.txt");
                 File.WriteAllText(initialFilePath, "");
                 await localRepo.StageAsync([initialFilePath]);
-                await localRepo.CommitAsync("Initial commit", allowEmpty: false, author: testIdentity);
+                await localRepo.CommitAsync("Initial commit", allowEmpty: false, author: authorIdentity);
 
                 // Create ancestor branch
                 await localRepo.CreateBranchAsync(AncestorBranch);
@@ -37,7 +44,7 @@ public sealed class LocalGitRepoHelperTests
                 var newFilePath = Path.Join(localRepo.Path, "newFile.txt");
                 File.WriteAllText(newFilePath, "");
                 await localRepo.StageAsync([newFilePath]);
-                await localRepo.CommitAsync("New commit", allowEmpty: false, author: testIdentity);
+                await localRepo.CommitAsync("New commit", allowEmpty: false, author: authorIdentity);
             });
 
         // Validate that repo helper correctly identifies ancestor/descendant relationship
