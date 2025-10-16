ef2e4536e76f9c35a7ff38c50b50110889b7a81d
Logan Bussell <loganbussell@microsoft.com>
2025-10-16 14:13:47 -0700

    Resolve .NET versions using `productVersions.txt` assets instead of `productCommit` asset (#6732)

--- eng/update-dependencies/ProductCommits.cs
+++ eng/update-dependencies/ProductCommits.cs
@@ -1,45 +1,44 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Text.Json;
 using System.Text.RegularExpressions;
+using Microsoft.DotNet.ProductConstructionService.Client.Models;
 
 namespace Dotnet.Docker;
 
 /// <summary>
-/// Represents the product commit versions for the SDK, runtime, and ASP.NET Core.
+/// Represents the product versions for the SDK, runtime, and ASP.NET Core.
 /// </summary>
-/// <example>
-/// {
-///   "runtime": { "commit": "721dc7a2a59416b21fc49447d264009d708d6000", "version": "10.0.0-preview.4.25223.119" },
-///   "aspnetcore": { "commit": "721dc7a2a59416b21fc49447d264009d708d6000", "version": "10.0.0-preview.5.25223.119" },
-///   "windowsdesktop": { "commit": "721dc7a2a59416b21fc49447d264009d708d6000", "version": "10.0.0-preview.5.25222.4" },
-///   "sdk": { "commit": "721dc7a2a59416b21fc49447d264009d708d6000", "version": "10.0.100-preview.5.25223.119" }
-/// }
-/// </example>
-internal partial record ProductCommits(
-    ProductCommit Sdk,
-    ProductCommit Runtime,
-    ProductCommit AspNetCore)
+internal partial record ProductVersions(
+    ProductVersion Sdk,
+    ProductVersion Runtime,
+    ProductVersion AspNetCore)
 {
-    private static readonly JsonSerializerOptions s_jsonOptions = new()
-    {
-        PropertyNameCaseInsensitive = true
-    };
-
-    [GeneratedRegex("^Sdk/.*/productCommit-linux-x64.json$")]
+    // Example: Sdk/10.0.100-rtm.25515.111/productVersion.txt
+    // Find full lists of assets at https://aka.ms/bar
+    [GeneratedRegex("^Sdk/.*/productVersion.txt$")]
     public static partial Regex SdkAssetRegex { get; }
 
-    public static ProductCommits FromJson(string json)
+    [GeneratedRegex("^aspnetcore/Runtime/.*/productVersion.txt$")]
+    public static partial Regex AspNetCoreAssetRegex { get; }
+
+    [GeneratedRegex("^Runtime/.*/productVersion.txt$")]
+    public static partial Regex RuntimeAssetRegex { get; }
+
+    public static ProductVersions FromVmrBuildAssets(IEnumerable<Asset> buildAssets) =>
+        new ProductVersions(
+            Sdk: GetAssetVersionMatchingRegex(buildAssets, SdkAssetRegex),
+            Runtime: GetAssetVersionMatchingRegex(buildAssets, RuntimeAssetRegex),
+            AspNetCore: GetAssetVersionMatchingRegex(buildAssets, AspNetCoreAssetRegex));
+
+    private static ProductVersion GetAssetVersionMatchingRegex(IEnumerable<Asset> buildAssets, Regex assetRegex)
     {
-        return JsonSerializer.Deserialize<ProductCommits>(json, s_jsonOptions)
+        Asset asset = buildAssets.FirstOrDefault(a => assetRegex.IsMatch(a.Name))
             ?? throw new InvalidOperationException(
-                $"""
-                Could not deserialize product commit versions from content:
-                {json}
-                """);
+                $"Could not find asset matching regex '{assetRegex}' in build assets.");
+
+        return new ProductVersion(asset.Version);
     }
 };
 
-internal record ProductCommit(string Commit, string Version);
+internal record struct ProductVersion(string Version);
