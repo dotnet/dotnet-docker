2a9e86205820d12d0a515f2dfe114015810583c3
Logan Bussell <loganbussell@microsoft.com>
2026-04-29 10:21:56 -0700

    Show detected remotes in Get-ReleaseBranches.ps1

--- .github/skills/shared/Get-ReleaseBranches.ps1
+++ .github/skills/shared/Get-ReleaseBranches.ps1
@@ -33,17 +33,28 @@ Write-Host "# Release branches"
 Write-Host "Release branches follow the Windows release naming scheme."
 Write-Host "Example: 2026-04B refers to the second week (B) of April 2026."
 
-$upstreamRemote = Get-RemoteName -UrlPattern 'github\.com[:/]dotnet/'
+$gitHubRemote = Get-RemoteName -UrlPattern 'github\.com[:/]dotnet/'
 $dncengRemote = Get-RemoteName -UrlPattern 'dev\.azure\.com/dnceng/'
+$upstreamRemoteUrl = git remote get-url $gitHubRemote
+$dncengRemoteUrl = git remote get-url $dncengRemote
 
-git fetch $upstreamRemote 2>&1 | Out-Null
+Write-Host ""
+Write-Host "## Repo configuration"
+Write-Host "This repo is configured with the following remotes:"
+Write-Host ""
+Write-Host "Source | Remote Name | Remote URL"
+Write-Host "--- | --- | ---"
+Write-Host "Public | $gitHubRemote | $upstreamRemoteUrl"
+Write-Host "Internal | $dncengRemote | $dncengRemoteUrl"
+
+git fetch $gitHubRemote 2>&1 | Out-Null
 git fetch $dncengRemote 2>&1 | Out-Null
 
 $numberOfBranches = 5
 
 Write-Host ""
 Write-Host "## ${numberOfBranches} most recent public release branches"
-git branch -r --list "$upstreamRemote/release/*" --sort=-creatordate | Select-Object -First $numberOfBranches
+git branch -r --list "$gitHubRemote/release/*" --sort=-creatordate | Select-Object -First $numberOfBranches
 
 Write-Host ""
 Write-Host "## ${numberOfBranches} most recent internal release branches"
