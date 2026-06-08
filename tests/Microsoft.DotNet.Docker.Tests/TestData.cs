2587bca4544c59dbe8b50f26e985d5226912977f
Logan Bussell <loganbussell@microsoft.com>
2026-06-08 05:23:28 -0700

    [nightly] Update Aspire Dashboard to 13.5 Preview (#7225)

--- tests/Microsoft.DotNet.Docker.Tests/TestData.cs
+++ tests/Microsoft.DotNet.Docker.Tests/TestData.cs
@@ -469,7 +469,7 @@ namespace Microsoft.DotNet.Docker.Tests
         private static readonly ProductImageData[] s_AspireDashboardTestData =
         {
             new() {
-                Version = V13_3,
+                Version = V13_5,
                 VersionFamily = V9_0,
                 OS = OS.AzureLinux30Distroless,
                 OSTag = "",
@@ -478,7 +478,7 @@ namespace Microsoft.DotNet.Docker.Tests
                 SupportedImageRepos = DotNetImageRepo.Aspire_Dashboard,
             },
             new() {
-                Version = V13_3,
+                Version = V13_5,
                 VersionFamily = V9_0,
                 OS = OS.AzureLinux30Distroless,
                 OSTag = "",
